bcfa63fde1c21e2d362ad9d673ef061325a7a5f4
zhouguangya <zhouguangya@teddymobile.cn>
2020-06-12 15:12:42 +0800

    1：学习了： ,await 让线程进入等待状态，notify 通知等待状态的线程进入运行转态；

--- java_thread/src/main/java/com/android/java_thread/PhaserDemo.java
+++ java_thread/src/main/java/com/android/java_thread/PhaserDemo.java
@@ -1,4 +1,14 @@
 package com.android.java_thread;
 
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.util.concurrent.Phaser;
+
 public class PhaserDemo {
+    Phaser mPhaser = new Phaser();
+    public static void main(String[] args) {
+        Object o = new Object();
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+    }
 }
