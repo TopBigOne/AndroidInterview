bddb47f3aa6eee9a59cd51f50bc003e6060a92ab
zhouguangya <zhouguangya@teddymobile.cn>
2020-06-17 10:50:24 +0800

    二分查找(被操作的数据，要求是有序的。)

--- Algorithm/src/main/java/ten_sort/BinarySearch.java
+++ Algorithm/src/main/java/ten_sort/BinarySearch.java
@@ -11,10 +11,10 @@ import java.util.Arrays;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] rawData = {12, 4, 6, 2, 7, 8, 3, 1, 6};
+        int[] rawData = {12, 4, 6, 2, 7, 1, 3, 8};
         Arrays.sort(rawData);
         System.out.println(" after sort arr:" + Arrays.toString(rawData));
-        int index = rank(6, rawData);
+        int index = getIndex4(12, rawData);
         System.out.println("index:" + index);
     }
 
@@ -24,18 +24,20 @@ public class BinarySearch {
      * @param rawData 数组必须是有序的。
      * @return
      */
-    public static int rank(int key, int[] rawData) {
+    private static int rank(int key, int[] rawData) {
         int lo = 0;
         int hi = rawData.length - 1;
         while (lo <= hi) {
             //  被查找的键要么不存在，要么必然存在于 a[o...hi] 之中；
-            int mid = lo + (hi - lo) / 2;
+            int mid = lo + (hi - lo) >> 1;
 
             // 与中间的那个数比较
             if (key < rawData[mid]) {
+                // 左边找
                 hi = mid - 1;
             } else if (key > rawData[mid]) {
-                hi = mid + 1;
+                // 右边找
+                lo = mid + 1;
             } else {
                 return mid;
             }
@@ -43,4 +45,80 @@ public class BinarySearch {
 
         return -1;
     }
+
+
+    private static int getTargetIndex(int target, int[] rawData) {
+        int lo = 0;
+        int hi = rawData.length - 1;
+        while (lo <= hi) {
+            //  每次取中间值
+            int mid = lo + (hi - lo) / 2;
+            if (target < rawData[mid]) {
+                hi = mid - 1;
+            } else if (target > rawData[mid]) {
+                lo = mid + 1;
+            } else {
+                return mid;
+            }
+
+        }
+
+        return -1;
+    }
+
+
+    private static int getIndex2(int target, int[] rawData) {
+        int low = 0;
+        int high = rawData.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) >> 1;
+            if (target < rawData[mid]) high = mid - 1;
+            else if (target > rawData[mid]) low = mid + 1;
+            else return mid;
+        }
+
+        return -1;
+    }
+
+    private static int getIndex3(int target, int[] rawData) {
+        int low = 0;
+        int high = rawData.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (target < rawData[mid]) high = mid - 1;
+            else if (target > rawData[mid]) low = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+
+    private static int getIndex4(int target, int[] rawData) {
+        int low = 0;
+        int high = rawData.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (target <= rawData[mid]) high = mid - 1;
+            if (target > rawData[mid]) low = mid + 1;
+            else return mid;
+        }
+        return -1;
+    }
+
+    private static int getIndex5(int target, int[] rawData) {
+        int low = 0;
+        int high = rawData.length - 1;
+        while (low <= high) {
+            int mid = low + ((high - low) >> 1);
+            if (target <= rawData[mid]) {
+                high = mid - 1;
+            } else if (target > rawData[mid]) {
+                low = mid + 1;
+            } else return mid;
+        }
+
+        return -1;
+    }
+
+
 }
---
+++ Algorithm/src/main/java/ten_sort/BitOperation.java
@@ -0,0 +1,20 @@
+package ten_sort;
+
+/**
+ * @author : Jakarta
+ * @version :
+ * @Date : 2020-06-16 14:32
+ * @Desc :
+ */
+public class BitOperation {
+    public static void main(String[] args) {
+
+
+        while (true) {
+            int low = 2;
+            int high = 10;
+            int reslut = low + ((high - low) >> 1);
+            System.out.println("result :" + reslut);
+        }
+    }
+}
