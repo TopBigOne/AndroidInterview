deab73290d8933ea24af8e12100b3048d5b16137
zhouguangya <zhouguangya@teddymobile.cn>
2020-07-01 12:08:00 +0800

    用一个栈实现另一个栈的排序;

--- Algorithm/src/main/java/com/top/jar/chapter_001/t_05/SortStack.java
+++ Algorithm/src/main/java/com/top/jar/chapter_001/t_05/SortStack.java
@@ -1,13 +1,61 @@
 package com.top.jar.chapter_001.t_05;
 
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.function.Consumer;
+
 /**
  * @author : Jakarta
  * @version :
  * @Date : 2020-06-20 23:49
- * @Desc : 用一个栈实现另一个栈的排序
+ * @Desc : 用一个栈实现另一个栈的排序;
  */
 public class SortStack {
     public static void main(String[] args) {
+        Stack<Integer> rawData = new Stack<>();
+        rawData.push(8);
+        rawData.push(3);
+        rawData.push(6);
+        rawData.push(4);
+        rawData.push(2);
+        rawData.push(5);
+        rawData.push(7);
+
+        printStack("raw   data: ", rawData);
+        Stack<Integer> resultStack = sortStack(rawData);
+        printStack("after sort: ", resultStack);
 
     }
+
+    private static Stack<Integer> sortStack(Stack<Integer> rawData) {
+        Stack<Integer> help = new Stack<>();
+        while (!rawData.isEmpty()) {
+            int temp = rawData.pop();
+            while (!help.isEmpty() && help.peek() < temp) {
+                rawData.push(help.pop());
+            }
+            help.push(temp);
+        }
+       /* while (!help.isEmpty()) {
+            rawData.push(help.pop());
+        }*/
+        System.out.println("help size: " + help.size());
+        return help;
+
+    }
+
+    private static void printStack(String tips, Stack<Integer> rawData) {
+        System.out.println(tips);
+
+        rawData.forEach(new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                System.out.println(integer);
+            }
+        });
+
+
+    }
+
+
 }
--- Algorithm/src/main/java/ten_sort/sort/SelectSort.java
+++ Algorithm/src/main/java/ten_sort/sort/SelectSort.java
@@ -11,16 +11,33 @@ import java.util.Arrays;
 public class SelectSort {
     public static void main(String[] args) {
         int[] arr = {2, 1, 6, 4, 5, 9, 0, 6};
-        sortData(arr);
+        sortData1(arr);
+        sortData2(arr);
+
         System.out.println("sort result:" + Arrays.toString(arr));
     }
 
-    private static void sortData(int[] arr) {
+    private static void sortData2(int[] arr) {
         int length = arr.length;
         for (int i = 0; i < length - 1; i++) {
             int minPos = i;
             for (int j = i + 1; j < length; j++) {
-                minPos = arr[j]<arr[minPos] ? j : minPos;
+
+
+
+            }
+
+        }
+
+
+    }
+
+    private static void sortData1(int[] arr) {
+        int length = arr.length;
+        for (int i = 0; i < length - 1; i++) {
+            int minPos = i;
+            for (int j = i + 1; j < length; j++) {
+                minPos = arr[j] < arr[minPos] ? j : minPos;
             }
             // swap...
             int temp = arr[i];
