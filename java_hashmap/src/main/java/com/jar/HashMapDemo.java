116ed9f1612d43ab6ebe48bee99df82f30f8db21
Big North <chinagyzhou@gmail.com>
2020-11-23 10:39:50 +0800

    1：测试 hashmap！

---
+++ java_hashmap/src/main/java/com/jar/HashMapDemo.java
@@ -0,0 +1,34 @@
+package com.jar;
+
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ *
+ */
+public class HashMapDemo {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+
+        test(6);
+    }
+
+    private static void test(int type) {
+
+        switch (type) {
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+
+            default:
+                System.out.println("异常");
+                return;
+        }
+
+        System.out.println(" 其他逻辑.....");
+
+    }
+}
