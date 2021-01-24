a970ff7eb596425657c5e2da23ee5e20b978f120
Big North <chinagyzhou@gmail.com>
2021-01-24 23:42:17 +0800

    1:最高薪资，练习了 3 遍！

--- Algorithm/src/main/java/dynamic_programming/opt_salary/Test.java
+++ Algorithm/src/main/java/dynamic_programming/opt_salary/Test.java
@@ -10,11 +10,12 @@ public class Test {
     public static void main(String[] args) {
         int[] startTime = {1, 3, 0, 4, 3, 5, 6, 8};
         int[] overTime = {4, 5, 6, 7, 8, 9, 10, 11};
+
         int[] salaries = {5, 1, 8, 4, 6, 3, 2, 4};
 
         OptSalary optSalary = new OptSalary();
-        int maxSalary = optSalary.maxSalary(startTime, overTime, salaries);
-        System.out.println("maxSalary: " + maxSalary);
+        int maxSalary = optSalary.maxSalary6(startTime, overTime, salaries);
+        System.out.println("maxSalary : " + maxSalary);
 
     }
 }
