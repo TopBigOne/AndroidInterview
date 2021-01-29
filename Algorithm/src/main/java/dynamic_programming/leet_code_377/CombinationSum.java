bd0487ecb74763a874cd86d15fc3aa6038b895a1
Big North <chinagyzhou@gmail.com>
2021-01-30 00:05:28 +0800

    1: combination sum  9 遍！

---
+++ Algorithm/src/main/java/dynamic_programming/leet_code_377/CombinationSum.java
@@ -0,0 +1,168 @@
+package dynamic_programming.leet_code_377;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  1/29/21 10:39 PM
+ * @Desc :  组合总和
+ * <p>
+ * <p>
+ * <p>
+ * Ⅳ
+ * <p>
+ * 给定一个由正整数组成且不存在重复数字的数组，找出和为给定目标正整数的组合的个数。
+ * <p>
+ * 示例:
+ * <p>
+ * nums = [1, 2, 3]
+ * target = 4
+ * <p>
+ * 所有可能的组合为：
+ * (1, 1, 1, 1)
+ * (1, 1, 2)
+ * (1, 2, 1)
+ * (1, 3)
+ * (2, 1, 1)
+ * (2, 2)
+ * (3, 1)
+ * <p>
+ * 请注意，顺序不同的序列被视作不同的组合。
+ * <p>
+ * 因此输出为 7。
+ * 进阶：
+ * 如果给定的数组中含有负数会怎么样？
+ * 问题会产生什么变化？
+ * 我们需要在题目中添加什么限制来允许负数的出现？
+ * 分析：https://leetcode-cn.com/problems/combination-sum-iv/solution/dong-tai-gui-hua-python-dai-ma-by-liweiwei1419/
+ */
+public class CombinationSum {
+    public int combination1(int[] nums, int target) {
+        // 为什么，要+1？
+        int[] dp = new int[target + 1];
+        // 这个值被其他状态参考，设置为 1 是合理的。
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+
+    public int combination2(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+
+            }
+
+        }
+        return dp[target];
+    }
+
+    public int combination3(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i < target + 1; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+    public int combination4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+
+        }
+        return dp[target];
+    }
+
+    public int combination5(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+
+        return dp[target];
+    }
+
+    public int combination6(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+
+        }
+        return dp[target];
+    }
+
+    public int combination7(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+
+        }
+        return dp[target];
+    }
+
+    public int combination8(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+        }
+        return dp[target];
+    }
+
+
+    public int combination9(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+        dp[0] = 1;
+        for (int i = 1; i <= target; i++) {
+            for (int num : nums) {
+                if (num <= i) {
+                    dp[i] += dp[i - num];
+                }
+            }
+
+        }
+        return dp[target];
+    }
+
+
+}
