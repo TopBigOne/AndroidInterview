a382a2930c4fcb162a10a2493d1af3cbab4e81fb
Big North <chinagyzhou@gmail.com>
2021-02-03 23:32:29 +0800

    1: 目标和（7 遍） (https://leetcode-cn.com/problems/target-sum/submissions/)

---
+++ Algorithm/src/main/java/dynamic_programming/leet_code_494/Test.java
@@ -0,0 +1,19 @@
+package dynamic_programming.leet_code_494;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2/3/21 5:44 PM
+ * @Desc :
+ */
+public class Test {
+    public static void main(String[] args) {
+        FindTargetSumWay findTargetSumWay = new FindTargetSumWay();
+        int[] nums = {1, 1, 1, 1, 1};
+        int s = 3;
+        int result = findTargetSumWay.findTargetSumWays1(nums, s);
+        System.out.println("result: "+result);
+
+
+    }
+}
