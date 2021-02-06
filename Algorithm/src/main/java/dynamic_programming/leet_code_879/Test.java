b8b60ec5d9b6cabb2433d328e356303e85835d66
Big North <chinagyzhou@gmail.com>
2021-02-07 01:04:32 +0800

    1: profitable schemes.

---
+++ Algorithm/src/main/java/dynamic_programming/leet_code_879/Test.java
@@ -0,0 +1,16 @@
+package dynamic_programming.leet_code_879;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2/6/21 12:12 AM
+ * @Desc :
+ */
+public class Test {
+    public static void main(String[] args) {
+        double a = 17;
+        double b = 3;
+        double result = a%b;
+        System.out.println("result: "+result);
+    }
+}
