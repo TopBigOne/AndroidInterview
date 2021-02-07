745cf18958931a3f44014668e6be963b6546c7ef
Big North <chinagyzhou@gmail.com>
2021-02-07 10:38:52 +0800

    1： 玩一下 two sum

---
+++ Algorithm/src/main/java/ten_sort/search/TwoSum.java
@@ -0,0 +1,40 @@
+package ten_sort.search;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.BiConsumer;
+
+import sun.security.provider.Sun;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2/7/21 1:07 AM
+ * @Desc :
+ */
+public class TwoSum {
+    public int[] sum(int[] nums, int target) {
+        int[] result = new int[2];
+        int length = nums.length;
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < length; i++) {
+            indexMap.put(i, nums[i]);
+        }
+
+        for (int i = 0; i < length; i++) {
+            int num = target - nums[i];
+            if (indexMap.containsValue(num)) {
+                result[0] = i;
+                for (Integer key : indexMap.keySet()) {
+                    if (indexMap.get(key) == num) {
+                        result[1] = key;
+                        break;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
+}
