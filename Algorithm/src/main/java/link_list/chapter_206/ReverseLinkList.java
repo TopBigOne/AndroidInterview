907e268b87f0cd67602b4bc2cd7a029791db11a3
Big North <chinagyzhou@gmail.com>
2021-02-08 23:00:48 +0800

    1: 反转链表！

--- Algorithm/src/main/java/link_list/chapter_206/ReverseLinkList.java
+++ Algorithm/src/main/java/link_list/chapter_206/ReverseLinkList.java
@@ -1,6 +1,8 @@
 package link_list.chapter_206;
 
 
+import java.util.List;
+
 import link_list.ListNode;
 
 /**
@@ -24,4 +26,95 @@ public class ReverseLinkList {
         }
         return cur;
     }
+
+    public ListNode reverseList2(ListNode head) {
+        ListNode cur = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode temp = pre.next;
+            pre.next = cur;
+            cur = pre;
+            pre = temp;
+        }
+        return cur;
+    }
+
+    public ListNode reverseList3(ListNode head) {
+        ListNode cur = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode temp = pre.next;
+            pre.next = cur;
+            cur = pre;
+            pre = temp;
+        }
+        return cur;
+    }
+
+    public ListNode reverList4(ListNode head) {
+        ListNode cur = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode temp = pre.next;
+            pre.next = cur;
+            cur = pre;
+            pre = temp;
+        }
+        return cur;
+    }
+
+    public ListNode reverseList5(ListNode head) {
+        ListNode cur = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode temp = pre.next;
+            pre.next = cur;
+            cur = pre;
+            pre = temp;
+        }
+        return cur;
+    }
+
+    public ListNode reverseList6(ListNode head) {
+        ListNode cur = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode temp = pre.next;
+            pre.next = cur;
+            cur = pre;
+            pre = temp;
+
+        }
+        return cur;
+    }
+
+
+    public ListNode reverseListNode7(ListNode head) {
+        ListNode curr = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode temp = pre.next;
+            pre.next = curr;
+            curr = pre;
+            pre = temp;
+        }
+
+        return curr;
+    }
+
+
+    public ListNode reverseListNode8(ListNode head) {
+        ListNode curr = null;
+        ListNode pre = head;
+        while (pre != null) {
+            ListNode temp = pre.next;
+            pre.next = curr;
+            curr = pre;
+            pre = temp;
+        }
+
+        return curr;
+    }
+
+
 }
