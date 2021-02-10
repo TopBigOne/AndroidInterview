08930c8cb583c13d16f53eff020c3a62a676a99a
Big North <chinagyzhou@gmail.com>
2021-02-10 22:37:20 +0800

    1：修改 rxjava 测试用例！

---
+++ 07_rxandroid/src/main/java/com/jar/rxandroid/MainActivity.java
@@ -0,0 +1,45 @@
+package com.jar.rxandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.ObservableEmitter;
+import io.reactivex.rxjava3.core.ObservableOnSubscribe;
+import io.reactivex.rxjava3.functions.Consumer;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        multiThread();
+
+
+    }
+
+    private void multiThread() {
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                e.onNext("This msg from work thread :" + Thread.currentThread().getName());
+               // sb.append("\nsubscribe: currentThreadName==" + Thread.currentThread().getName());
+            }
+        }).subscribeOn(Schedulers.newThread())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<String>() {
+                    @Override
+                    public void accept(String s) throws Exception {
+                        Log.d(TAG, "accept: : " + s);
+                    }
+                });
+    }
+
+
+}
