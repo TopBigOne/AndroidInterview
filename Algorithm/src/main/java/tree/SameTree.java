6ee7c1f3fd8569f4423bfd5358afd9bd563e532d
Big North <chinagyzhou@gmail.com>
2021-02-13 02:19:12 +0800

    如何判断两颗树 是否相等？

--- Algorithm/src/main/java/tree/SameTree.java
+++ Algorithm/src/main/java/tree/SameTree.java
@@ -13,18 +13,23 @@ public class SameTree {
 
     }
 
-    public boolean isSameTree(TreeNode root1, TreeNode root2) {
-        if (root1 == null && root2 == null) {
+    public boolean isSameTree(TreeNode root) {
+        return isSameTree(root, null, null);
+    }
+
+
+    public boolean isSameTree(TreeNode root, TreeNode min, TreeNode max) {
+        if (root == null) {
             return true;
         }
-        if (root1 == null || root2 == null) {
+        if (min != null && root.val <= min.val) {
             return false;
         }
-        if (!root1.val.equals(root2.val)) {
+        if (max.val != null && root.val >= max.val) {
             return false;
         }
         // 卧槽，，全是递归，，
-        return isSameTree(root1.left, root2.right) && isSameTree(root1.right, root2.right);
+        return isSameTree(root.left, min, root) && isSameTree(root.right, root, max);
 
     }
 }
