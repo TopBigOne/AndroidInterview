c15da9f1dc71827226621ef2bf6490025b643357
Big North <chinagyzhou@gmail.com>
2021-02-13 19:03:39 +0800

    删除 bst 的某一个节点；

--- Algorithm/src/main/java/tree/OpreationTree.java
+++ Algorithm/src/main/java/tree/OpreationTree.java
@@ -61,5 +61,51 @@ public class OpreationTree {
         return isInBST(root.right, target);
     }
 
+    /**
+     * 删除 bst 的某一个节点（https://leetcode-cn.com/problems/delete-node-in-a-bst/solution/chao-xiang-xi-zhu-shi-ji-bai-9951-9896-b-2dqs/）
+     *
+     * @param root
+     * @param key
+     * @return
+     */
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == key) {
+            // 情景 1： A 恰好是末端节点，两个子节点都为空，那么它就可以立即退场了。
+            // 情景 2： A 只有一个非空节点，那么他要让这个孩子接替自己的位置；
+            if (root.left == null) {
+                return root.right;
+            }
+            if (root.right == null) {
+                return root.left;
+            }
+            // 情景 3： A 有 2 个节点，为了不破坏 BST 的性质，A 必须找打左子树中最大的那个节点，或者右子树中最小的那个几点来替代自己，
+            //         我们就可以按照第二种方式，去找右子树中最小的那个节点来接替 待删除节点。
+            // 获取右子树最小的节点来替换 待删除的节点
+            TreeNode minNode = getMin(root.right);
+            //
+            root.val = minNode.val;
+            root.right = deleteNode(root.right, minNode.val);
+        } else if (root.val > key) {
+            root.left = deleteNode(root.left, key);
+        } else if (root.val < key) {
+            root.right = deleteNode(root.right, key);
+        }
+
+        return root;
+    }
+
+    private TreeNode getMin(TreeNode node) {
+        // bst 最左的那个就是最小的
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
+
+
+    }
+
 
 }
