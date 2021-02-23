22e5b981b9a8b343d5173938049ede8160088695
Big North <chinagyzhou@gmail.com>
2021-02-24 00:40:36 +0800

    以右视图的方式，打印二叉树的节点，，，

---
+++ Algorithm/src/main/java/tree/bfs/leet_code_199/RightSideView.java
@@ -0,0 +1,80 @@
+package tree.bfs.leet_code_199;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import tree.TreeNode;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2/23/21 11:39 PM
+ * @Desc : (https://leetcode-cn.com/problems/binary-tree-right-side-view/submissions/)
+ * <p>
+ * 主要考察 bsf
+ * <p>
+ * 题解：https://leetcode-cn.com/problems/binary-tree-right-side-view/solution/er-cha-shu-de-you-shi-tu-by-leetcode-solution/
+ */
+public class RightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+                if (temp == null) {
+                    continue;
+                }
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+                if (i == size - 1) {
+                    result.add(temp.val);
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> rightSideView2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode temp = queue.poll();
+                if (temp == null) {
+                    continue;
+                }
+                if (temp.left != null) {
+                    queue.offer(temp.left);
+                }
+                if (temp.right != null) {
+                    queue.offer(temp.right);
+                }
+                if (i == size - 1) {
+                    result.add(temp.val);
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+
+}
