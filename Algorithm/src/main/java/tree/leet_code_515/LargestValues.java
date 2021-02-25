cc99ab55271a2389e6c1e16f97ab71141741cafc
Big North <chinagyzhou@gmail.com>
2021-02-25 22:12:29 +0800

    1:在每个树行中找最大值

---
+++ Algorithm/src/main/java/tree/leet_code_515/LargestValues.java
@@ -0,0 +1,18 @@
+package tree.leet_code_515;
+
+import java.util.List;
+
+import tree.TreeNode;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2/25/21 10:11 PM
+ * @Desc :  在每个树行中找最大值 （https://leetcode-cn.com/problems/find-largest-value-in-each-tree-row/）
+ */
+public class LargestValues {
+    public List<Integer> largestValues(TreeNode root) {
+        return  null;
+
+    }
+}
