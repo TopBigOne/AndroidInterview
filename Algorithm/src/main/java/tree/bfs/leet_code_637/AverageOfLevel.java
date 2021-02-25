b4d9466ec0969e7858b846cdf2d2f9d8b80230ce
Big North <chinagyzhou@gmail.com>
2021-02-25 22:05:40 +0800

    1：二叉树的层平均值

--- Algorithm/src/main/java/tree/bfs/leet_code_637/AverageOfLevel.java
+++ Algorithm/src/main/java/tree/bfs/leet_code_637/AverageOfLevel.java
@@ -1,12 +1,10 @@
-package tree.leet_code_637;
+package tree.bfs.leet_code_637;
 
 
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
-import sun.security.provider.Sun;
 import tree.TreeNode;
 
 /**
