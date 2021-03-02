a3c16040fa2509ac4c782a34a2bff0ee2c9e5b3f
Big North <chinagyzhou@gmail.com>
2021-03-02 18:45:12 +0800

    1 ： 递归的方式：树的中序遍历；

---
+++ Algorithm/src/main/java/stack/leet_code_94/InorderTraversal.java
@@ -0,0 +1,49 @@
+package stack.leet_code_94;
+
+import com.top.jar.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  3/2/21 6:15 PM
+ * @Desc : 二叉树的中序遍历（https://leetcode-cn.com/problems/binary-tree-inorder-traversal/）
+ */
+public class InorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        helper(result, root);
+        return result;
+    }
+
+    private void helper(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper(list, root.left);
+        list.add(root.value);
+        helper(list, root.right);
+    }
+
+    private void helper2(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper2(list, root.left);
+        list.add(root.value);
+        helper2(list, root.right);
+
+    }
+
+    private void helper3(List<Integer> list, TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        helper3(list, root.left);
+        list.add(root.value);
+        helper3(list, root.right);
+    }
+}
