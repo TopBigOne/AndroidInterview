009ccb2fc0800ca2838c18a44a65f006bebcaccb
Big North <chinagyzhou@gmail.com>
2021-03-11 10:37:13 +0800

    jvm-classloader

--- java_class_loader/src/main/java/com/example/java_class_loader/MyClass.java
+++ java_class_loader/src/main/java/com/example/java_class_loader/MyClass.java
@@ -1,4 +1,15 @@
 package com.example.java_class_loader;
 
 public class MyClass {
+    public static void main(String[] args) {
+        MyClass myClass = new MyClass();
+        myClass.testClassName();
+
+    }
+
+    public void testClassName(){
+        String simpleName = this.getClass().getSimpleName();
+        System.out.println("simpleName:"+simpleName);
+
+    }
 }
