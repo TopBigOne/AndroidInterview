1716d6d488268488fdf37bfe678603144b3f1b53
Big North <chinagyzhou@gmail.com>
2021-03-14 20:37:22 +0800

    1: 移掉k位数字；

---
+++ Algorithm/src/main/java/stack/single_invoke_stack/leet_code_402/RemoveKDigit.java
@@ -0,0 +1,10 @@
+package stack.single_invoke_stack.leet_code_402;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  3/14/21 8:35 PM
+ * @Desc : 移掉K位数字 https://leetcode-cn.com/problems/remove-k-digits/
+ */
+public class RemoveKDigit {
+}
