844200814d10f7cbf1e1e335f276340a50df54ba
Big North <chinagyzhou@gmail.com>
2021-03-14 22:21:52 +0800

    1:整数反转

---
+++ Algorithm/src/main/java/stack/leet_code_7/ReverseInt.java
@@ -0,0 +1,27 @@
+package stack.leet_code_7;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  3/14/21 9:58 PM
+ * @Desc :
+ */
+public class ReverseInt {
+    public int reverse(int x) {
+        if (x == 0) {
+            return 0;
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        ReverseInt reverseInt = new ReverseInt();
+        int result1 = reverseInt.reverse(9646324351);
+    }
+}
