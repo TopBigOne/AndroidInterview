8f06f116a51168bb8a553523f2673630be68184b
Big North <chinagyzhou@gmail.com>
2021-03-17 01:51:03 +0800

    1:简化路径

--- Algorithm/src/main/java/stack/single_invoke_stack/leet_code_71/SimplifyPath.java
+++ Algorithm/src/main/java/stack/single_invoke_stack/leet_code_71/SimplifyPath.java
@@ -1,7 +1,9 @@
 package stack.single_invoke_stack.leet_code_71;
 
+import java.text.DecimalFormat;
 import java.util.Deque;
 import java.util.LinkedList;
+import java.util.Queue;
 
 /**
  * @author : dev
@@ -9,7 +11,39 @@ import java.util.LinkedList;
  * @Date :  3/16/21 9:05 PM
  * @Desc :  简化路径（https://leetcode-cn.com/problems/simplify-path/solution/）
  * 题解：https://leetcode-cn.com/problems/simplify-path/solution/java-yi-dong-yi-jie-xiao-lu-gao-by-spirit-9-18/
+ *
+ * 请注意，返回的 规范路径 必须遵循下述格式：
+ *
+ * 始终以斜杠 '/' 开头。
+ * 两个目录名之间必须只有一个斜杠 '/' 。
+ * 最后一个目录名（如果存在）不能 以 '/' 结尾。
+ * 此外，路径仅包含从根目录到目标文件或目录的路径上的目录（即，不含 '.' 或 '..'）。
+ * 返回简化后得到的 规范路径 。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：path = "/home/"
+ * 输出："/home"
+ * 解释：注意，最后一个目录名后面没有斜杠。
+ * 示例 2：
+ *
+ * 输入：path = "/../"
+ * 输出："/"
+ * 解释：从根目录向上一级是不可行的，因为根目录是你可以到达的最高级。
+ * 示例 3：
+ *
+ * 输入：path = "/home//foo/"
+ * 输出："/home/foo"
+ * 解释：在规范路径中，多个连续斜杠需要用一个斜杠替换。
+ * 示例 4：
+ *
+ * 输入：path = "/a/./b/../../c/"
+ * 输出："/c"
  */
+
+
 public class SimplifyPath {
 
     public String simplifyPath(String path) {
@@ -66,6 +100,160 @@ public class SimplifyPath {
         }
 
         return sb.toString();
+    }
+
+    public String simplifyPath3(String path) {
+        String SPLIT_SYMBLE_LEFT = "/";
+        String SPLIT_SYMBLE_ONE = ".";
+        String SPLIT_SYMBLE_TWO = "..";
+        String[] dirs = path.split(SPLIT_SYMBLE_LEFT);
+        Deque<String> deque = new LinkedList<>();
+        for (String dir : dirs) {
+            if (!deque.isEmpty() && SPLIT_SYMBLE_TWO.equals(dir)) {
+                deque.pop();
+                continue;
+            }
+            if (!"".equals(dir)
+                    && !SPLIT_SYMBLE_ONE.equals(dir)
+                    && !SPLIT_SYMBLE_TWO.equals(dir)) {
+                deque.push(dir);
+            }
+
+        }
+
+        if (deque.isEmpty()) {
+            return SPLIT_SYMBLE_LEFT;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append(SPLIT_SYMBLE_LEFT).append(deque.pollLast());
+
+        }
+
+        return sb.toString();
+    }
+
+
+    public String simplifyPath4(String path) {
+        String left = "/";
+        String noneDot = "";
+        String oneDot = ".";
+        String twoDot = "..";
+        Deque<String> deque = new LinkedList<>();
+        String[] dirs = path.split(left);
+        for (String dir : dirs) {
+            if (!deque.isEmpty() && twoDot.equals(dir)) {
+                deque.pop();
+                continue;
+            }
+
+            if (!noneDot.equals(dir) && !oneDot.equals(dir) && !twoDot.equals(dir)) {
+                deque.push(dir);
+            }
+        }
+
+        if (deque.isEmpty()) {
+            return left;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append(left).append(deque.pollLast());
+
+        }
+
+        return sb.toString();
+    }
+
+    public String simplifyPath5(String path) {
+        String lef = "/";
+        String nodeDot = "";
+        String oneDot = ".";
+        String twoDot = "..";
+        String[] dirs = path.split(lef);
+        Deque<String> queue = new LinkedList<>();
+        for (String dir : dirs) {
+            if (!queue.isEmpty() && twoDot.equals(dir)) {
+                queue.pop();
+                continue;
+            }
+            if (!nodeDot.equals(dir) && !oneDot.equals(dir) && !twoDot.equals(dir)) {
+                queue.push(dir);
+            }
+        }
+        if (queue.isEmpty()) {
+            return lef;
+        }
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            sb.append(lef).append(queue.pollLast());
+
+        }
+        return sb.toString();
+    }
+
+    public String simplifyPath6(String path) {
+        String left = "/";
+        String noneDot = "";
+        String oneDot = ".";
+        String twoDot = "..";
+        Deque<String> deque = new LinkedList<>();
+        String[] dirs = path.split(left);
+        for (String dir : dirs) {
+            if (!deque.isEmpty() && twoDot.equals(dir)) {
+                deque.pop();
+                continue;
+            }
+            if (!noneDot.equals(dir) && !oneDot.equals(dir) && !twoDot.equals(dir)) {
+                deque.push(dir);
+            }
+        }
+
+        if (deque.isEmpty()) {
+            return left;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!deque.isEmpty()) {
+            sb.append(left).append(deque.pollLast());
+
+        }
+
+        return sb.toString();
+
+    }
+
+
+    public String simplifyPath7(String path) {
+        String left = "/";
+        String noneDot = "";
+        String oneDot = ".";
+        String twoDot = "..";
+        Deque<String> queue = new LinkedList<>();
+        String[] dirs = path.split(left);
+        for (String dir : dirs) {
+            if (!queue.isEmpty() && twoDot.equals(dir)) {
+                queue.pop();
+                continue;
+            }
+            if (!noneDot.equals(dir) && !oneDot.equals(dir) &&! twoDot.equals(dir)) {
+                queue.push(dir);
+            }
+
+        }
+
+
+        if (queue.isEmpty()) {
+            return left;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (!queue.isEmpty()) {
+            sb.append(left).append(queue.pollLast());
+        }
+
+        return sb.toString();
+
 
     }
 
