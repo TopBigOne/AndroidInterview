6e513ec4b15a1e031c42c09504d5999565c5564c
Big North <chinagyzhou@gmail.com>
2021-03-22 17:21:38 +0800

    通过数的前序和中序遍历，还原一颗树

---
+++ Algorithm/src/main/java/tree/leet_code_105/BuildTree.java
@@ -0,0 +1,18 @@
+package tree.leet_code_105;
+
+import com.top.jar.TreeNode;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/3/19 21:26
+ * @Desc : 字节：从前序与中序遍历的结果，构造二叉树（https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/）
+ */
+public class BuildTree {
+    public TreeNode buildTree(int [] preorder, int [] inorder){
+
+
+        return  null;
+    }
+
+}
