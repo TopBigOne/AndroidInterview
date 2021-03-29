56903f4f42fe4dd96491701d68bdd0fd5bbb300f
Big North <chinagyzhou@gmail.com>
2021-03-30 01:24:17 +0800

    最长公共子序列！

--- Algorithm/src/main/java/dp/leet_code_1143/LongestCommonSubsequence.java
+++ Algorithm/src/main/java/dp/leet_code_1143/LongestCommonSubsequence.java
@@ -1,11 +1,14 @@
 package dp.leet_code_1143;
 
+import sun.jvm.hotspot.memory.Space;
+
 /**
  * @author : dev
  * @version :
  * @Date :  2/17/21 4:04 PM
  * @Desc : 最长公共子序列（https://leetcode-cn.com/problems/longest-common-subsequence/solution/）
  * <p>
+ * 字节半年，出现了 13 次；
  * 题解：https://leetcode-cn.com/problems/longest-common-subsequence/solution/dong-tai-gui-hua-tu-wen-jie-xi-by-yijiaoqian/
  * dp: 思路：
  * 1： 明确 dp 数组的含义；
@@ -103,5 +106,62 @@ public class LongestCommonSubsequence {
         }
         return dp[m][n];
     }
+
+    private int longCommonSubsequence6(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char c1 = text1.charAt(i);
+                char c2 = text2.charAt(j);
+                if (c1 == c2) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    continue;
+                }
+                dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+
+            }
+        }
+        return dp[m][n];
+    }
+
+    public int longCommonSubSequence7(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char c1 = text1.charAt(i);
+                char c2 = text2.charAt(j);
+                if (c1 == c2) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    continue;
+                }
+                dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
+            }
+        }
+        return dp[m][n];
+    }
+
+    public int longCommonSubsequence8(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        // 必须要加1；
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char c1 = text1.charAt(i);
+                char c2 = text2.charAt(j);
+                if (c1 == c2) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    continue;
+                }
+                dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+        return dp[m][n];
+    }
+
 
 }
