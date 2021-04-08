a9e2340c281f3f05fa579d79e26db94473d80e9e
Big North <chinagyzhou@gmail.com>
2021-04-08 20:32:10 +0800

    1:最长公共子序列

--- Algorithm/src/main/java/dp/leet_code_1143/LongestCommonSubsequence.java
+++ Algorithm/src/main/java/dp/leet_code_1143/LongestCommonSubsequence.java
@@ -1,7 +1,5 @@
 package dp.leet_code_1143;
 
-import sun.jvm.hotspot.memory.Space;
-
 /**
  * @author : dev
  * @version :
@@ -89,7 +87,7 @@ public class LongestCommonSubsequence {
         return dp[m][n];
     }
 
-    public int longestCommonSubseauence5(String text1, String text2) {
+    public int longestSubsequence5(String text1, String text2) {
         int m = text1.length();
         int n = text2.length();
         int[][] dp = new int[m + 1][n + 1];
@@ -161,6 +159,25 @@ public class LongestCommonSubsequence {
             }
         }
         return dp[m][n];
+    }
+
+    public int longCommonSubsequence9(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                char c1 = text1.charAt(i);
+                char c2 = text2.charAt(j);
+                if (c1 == c2) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                    continue;
+                }
+                dp[i + 1][j + 1] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+            }
+        }
+        return dp[m][n];
+
     }
 
 
