1870a7be93d90321deb4a1e747d8e33464d2acc5
Big North <chinagyzhou@gmail.com>
2021-04-08 20:39:36 +0800

    位运算；

--- java_base/src/main/java/com/android/base/BitArithmetic.java
+++ java_base/src/main/java/com/android/base/BitArithmetic.java
@@ -16,8 +16,19 @@ public class BitArithmetic {
     public static void main(String[] args) {
         int a = 5 & 3;
         System.out.println("5 & 3:" + a);
+        /*
+        * 01-1
+        * 10-2
+        * 11-3
+        * 100-4
+        * 0000 0101-5  ---取反-->1111 1010--->2
+        * 所有正整数的按位取反是其 本身+1的 负数
+        * 所有负整数的按位取反是其 本身+1的绝对值
+        * 零的按位取反是 -1
+        * */
         System.out.println("~5 :" + ~5);
         System.out.println("~1 :" + ~1);
+        System.out.println("~0 :" + ~0);
         System.out.println("0^0 :" + (0^0));
     }
 }
