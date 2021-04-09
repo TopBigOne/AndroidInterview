7faf6b6255fd6531c42275fd342bd06562a69e8b
Big North <chinagyzhou@gmail.com>
2021-04-10 01:19:53 +0800

    整数反转

---
+++ Algorithm/src/main/java/math/leet_code_7/Test.java
@@ -0,0 +1,16 @@
+package math.leet_code_7;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  3/27/21 11:02 PM
+ * @Desc : 半年之内，这道题，字节出了 23 次
+ */
+public class Test {
+    public static void main(String[] args) {
+        Reverse reverse = new Reverse();
+        int result = reverse.reverse(6546558);
+        System.out.println("result : "+result);
+
+    }
+}
