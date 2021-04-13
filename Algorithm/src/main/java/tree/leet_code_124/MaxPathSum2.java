de8e19c4b7fc6a83293f20bd6421bbc51e49ac2a
Big North <chinagyzhou@gmail.com>
2021-04-14 01:07:06 +0800

    1: 二叉树的最大路径；

--- Algorithm/src/main/java/tree/leet_code_124/MaxPathSum2.java
+++ Algorithm/src/main/java/tree/leet_code_124/MaxPathSum2.java
@@ -16,11 +16,60 @@ public class MaxPathSum2 {
     int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
 
-        dfs(root);
+        dfs1(root);
         return  max;
 
+    }
+    /**
+     * 返回经过root的单边分支最大和， 即Math.max(root, root+left, root+right)
+     * @param root
+     * @return
+     */
+    public int dfs1(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        //计算左边分支最大值，左边分支如果为负数还不如不选择
+        int leftMax = Math.max(0, dfs1(root.left));
+        //计算右边分支最大值，右边分支如果为负数还不如不选择
+        int rightMax = Math.max(0, dfs1(root.right));
+        //left->root->right 作为路径与已经计算过历史最大值做比较
+        max = Math.max(max, root.val + leftMax + rightMax);
+        // 返回经过root的单边最大分支给当前root的父节点计算使用
+        return root.val + Math.max(leftMax, rightMax);
+    }
+
+    private int dfs2(TreeNode root) {
+        if (root==null) {
+            return 0;
+        }
+        int leftMax = Math.max(0,dfs2(root.left));
+        int rightMax = Math.max(0,dfs2(root.right));
+        max = Math.max(max,root.val+leftMax+rightMax);
+        return root.val+Math.max(leftMax,rightMax);
 
     }
+
+    private  int dfs3(TreeNode root){
+        if (root==null) {
+            return 0;
+        }
+        int leftMax = Math.max(0,dfs3(root.left));
+        int rightMax = Math.max(0,dfs3(root.right));
+        max = Math.max(max,root.val+leftMax+rightMax);
+        return root.val+Math.max(leftMax,rightMax);
+    }
+
+    private  int dfs4(TreeNode root){
+        if(root==null){
+            return 0;
+        }
+        int leftMax = Math.max(0,dfs4(root.left));
+        int rightMax = Math.max(0,dfs4(root.right));
+        max = Math.max(max,root.val+leftMax+rightMax);
+        return  root.val+Math.max(leftMax,rightMax);
+    }
+
 
 
 }
