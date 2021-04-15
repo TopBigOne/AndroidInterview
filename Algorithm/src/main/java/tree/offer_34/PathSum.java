cdcd79ff205f83f102a449bbb754a16e7d75d34a
Big North <chinagyzhou@gmail.com>
2021-04-15 21:50:13 +0800

    1: 二叉树中 ，某一值的路径；

---
+++ Algorithm/src/main/java/tree/offer_34/PathSum.java
@@ -0,0 +1,57 @@
+package tree.offer_34;
+
+import tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/4/15 20:40
+ * @Desc : （半年，字节出现了7次）二叉树中，某一值的路径（https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/）
+ * <p>
+ * 输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
+ * 示例:
+ * 给定如下二叉树，以及目标和 target = 22，
+ * <p>
+ * 5
+ * / \
+ * 4   8
+ * /   / \
+ * 11  13  4
+ * /  \    / \
+ * 7    2  5   1
+ * 返回:
+ * <p>
+ * [
+ * [5,4,11,2],
+ * [5,8,4,5]
+ * ]
+ * 题解：https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof/solution/mian-shi-ti-34-er-cha-shu-zhong-he-wei-mou-yi-zh-5/
+ */
+public class PathSum {
+    private final List<List<Integer>> res = new LinkedList<>();
+    private final LinkedList<Integer> path = new LinkedList<>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int target) {
+        recur(root, target);
+        return res;
+    }
+
+    private void recur(TreeNode root, int target) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        // 牛B，我他妈还是第一次见这种写法；
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null) {
+            res.add(new LinkedList<>(path));
+        }
+        recur(root.left, target);
+        recur(root.right, target);
+        path.removeLast();
+    }
+
+}
---
+++ Algorithm/src/main/java/tree/offer_34/PathSum2.java
@@ -0,0 +1,57 @@
+package tree.offer_34;
+
+import tree.TreeNode;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/4/15 21:01
+ * @Desc :
+ */
+public class PathSum2 {
+    private final LinkedList<List<Integer>> res  = new LinkedList<>();
+    private  final LinkedList<Integer> path = new LinkedList<>();
+
+
+    public List<List<Integer>> pathSum(TreeNode root,int target){
+        recur(root,target);
+        return res;
+    }
+
+    private void recur(TreeNode root, int target) {
+        if(root==null){
+            return;
+        }
+        path.add(root.val);
+        target-=root.val;
+        if(target==0&&root.left==null&&root.right==null){
+            res.add(new LinkedList<>(path));
+        }
+        recur(root.left, target);
+        recur(root.right, target);
+        path.removeLast();
+
+    }
+
+    private  void recur2(TreeNode root,int target){
+        if(root==null){
+            return ;
+        }
+
+        path.add(root.val);
+        target-=root.val;
+        if(target==0&&root.left==null&&root.right==null){
+            res.add(new LinkedList<>(path));
+        }
+        recur(root.left,target);
+        recur(root.right, target);
+        path.removeLast();
+
+    }
+
+
+
+}
