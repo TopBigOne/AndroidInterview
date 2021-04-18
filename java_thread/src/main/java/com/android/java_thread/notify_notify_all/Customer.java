d60d43cce080d2ea9d1950bde2782a1363cc4561
Big North <chinagyzhou@gmail.com>
2021-04-18 21:19:15 +0800

    1：多线程中，消费者和生产者模型；

---
+++ java_thread/src/main/java/com/android/java_thread/notify_notify_all/Customer.java
@@ -0,0 +1,42 @@
+package com.android.java_thread.notify_notify_all;
+
+import java.util.List;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  4/18/21 8:30 PM
+ * @Desc :
+ */
+public class Customer implements Runnable {
+    private List<Integer> cache;
+
+    public Customer(List<Integer> cache) {
+        this.cache = cache;
+    }
+
+    private void custom() {
+        synchronized (cache) {
+            try {
+                while (cache.size() == 0) {
+                    cache.wait();
+                }
+                cache.remove(0);
+                System.out.println(Thread.currentThread().getName() + " 消费者了消费一条");
+                cache.notify();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            custom();
+        }
+    }
+
+
+}
---
+++ java_thread/src/main/java/com/android/java_thread/notify_notify_all/Test.java
@@ -0,0 +1,18 @@
+package com.android.java_thread.notify_notify_all;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  4/18/21 8:45 PM
+ * @Desc :
+ */
+public class Test {
+    public static void main(String[] args) {
+        List<Integer> cache = new ArrayList<>();
+        new Thread(new Producer(cache),"P1").start();
+        new Thread(new Customer(cache),"C1").start();
+    }
+}
