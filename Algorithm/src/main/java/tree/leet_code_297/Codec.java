820f94f8b486f1ed2dafbc67c68022efb6cd5369
Big North <chinagyzhou@gmail.com>
2021-04-18 21:11:40 +0800

    1：二叉树的序列化与反序列化！

---
+++ Algorithm/src/main/java/tree/leet_code_297/Codec.java
@@ -0,0 +1,53 @@
+package tree.leet_code_297;
+
+import org.omg.CORBA.INTERNAL;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Objects;
+import java.util.TreeMap;
+
+import tree.TreeNode;
+import tree.offer_07.BuildTree;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  4/17/21 5:58 PM
+ * @Desc :
+ * <p>
+ * https://leetcode-cn.com/problems/serialize-and-deserialize-binary-tree/solution/shou-hui-tu-jie-gei-chu-dfshe-bfsliang-chong-jie-f/
+ */
+
+
+public class Codec {
+
+    public String serialize(TreeNode root) {
+        if (root == null) {
+            return "X,";
+        }
+        String leftSerialize = serialize(root.left);
+        String rightSerialize = serialize(root.right);
+        return root.val + "," + leftSerialize + rightSerialize;
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] temp = data.split(",");
+        Deque<String> dp = new LinkedList<>(Arrays.asList(temp));
+        return buildTree(dp);
+    }
+
+    private TreeNode buildTree(Deque<String> dp) {
+        // 返回当前节点
+        String s = dp.poll();
+        if (s.equals("X")) {
+            return null;
+        }
+        TreeNode root = new TreeNode(Integer.parseInt(s));
+        root.left = buildTree(dp);
+        root.right = buildTree(dp);
+        return root;
+    }
+}
