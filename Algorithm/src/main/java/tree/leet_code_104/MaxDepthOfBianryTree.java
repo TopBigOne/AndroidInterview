1b9f59531f86b040e96376d438f02b180adaf5b5
Big North <chinagyzhou@gmail.com>
2021-04-18 21:10:46 +0800

    1：二叉树的最大深度；

--- Algorithm/src/main/java/tree/leet_code_104/MaxDepthOfBianryTree.java
+++ Algorithm/src/main/java/tree/leet_code_104/MaxDepthOfBianryTree.java
@@ -6,7 +6,7 @@ import tree.TreeNode;
  * @author : dev
  * @version :
  * @Date :  2/12/21 3:07 PM
- * @Desc : 二叉树的最大深度（https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/）
+ * @Desc : 二叉树的最大深度 字节出现 8 次（https://leetcode-cn.com/problems/maximum-depth-of-binary-tree/）
  * 考察深度优先搜索
  * <p>
  * 给定一个二叉树，找出其最大深度。
@@ -18,10 +18,10 @@ import tree.TreeNode;
  * 示例：
  * 给定二叉树 [3,9,20,null,null,15,7]，
  * <p>
- * 3
- * / \
- * 9  20
- * /  \
+ *    3
+ *   / \
+ *  9  20
+ *  /  \
  * 15   7
  * 返回它的最大深度 3 。
  */
@@ -60,8 +60,16 @@ public class MaxDepthOfBianryTree {
         }
         int left = maxDepth(root.left);
         int right = maxDepth(root.right);
-
         return Math.max(left, right) + 1;
+    }
+
+    public int maxDepth4(TreeNode root){
+        if (root==null) {
+            return 0;
+        }
+        int left = maxDepth4(root.left);
+        int right = maxDepth4(root.right);
+        return Math.max(left,right)+1;
 
     }
 
