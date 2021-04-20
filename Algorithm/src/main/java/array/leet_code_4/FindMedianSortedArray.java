dace1463558b94c4d642b734684b5852b4c3e9a6
Big North <chinagyzhou@gmail.com>
2021-04-21 00:13:29 +0800

    1：寻找两个正序数组的中位数；

--- Algorithm/src/main/java/array/leet_code_4/FindMedianSortedArray.java
+++ Algorithm/src/main/java/array/leet_code_4/FindMedianSortedArray.java
@@ -1,13 +1,17 @@
 package array.leet_code_4;
 
+import org.omg.CORBA.INTERNAL;
+
+import javax.sound.midi.Soundbank;
+
 /**
  * @author : dev
  * @version :
  * @Date :  2021/4/20 10:19
- * @Desc :
+ * @Desc : 寻找两个正序数组的中位数
  * <p>
  * 题解：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
- *
+ * <p>
  * https://blog.csdn.net/chen_xinjia/article/details/69258706
  */
 public class FindMedianSortedArray {
@@ -107,48 +111,133 @@ public class FindMedianSortedArray {
         return 0.0;
     }
 
-    public static double findMedianSortedArrays3(int[] nums1, int[] nums2) {
-        int MIN_VALUE = 0x80000000;
-
-        int MAX_VALUE = 0x7fffffff;
-
-        int N1 = nums1.length;
-        int N2 = nums2.length;
-        if (N1 > N2) {// 确保N1是短的部分。
+    public double findMedianSortedArrays3(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) {// 确保N1是短的部分。
             return findMedianSortedArrays3(nums2, nums1);
         }
 
-        if (N1 == 0)
-            return ((double) nums2[(N2 - 1) / 2] + (double) nums2[N2 / 2]) / 2;
-        int size = N1 + N2;
-        int cutL = 0, cutR = N1;
-        int cut1 = N1 / 2;
-        int cut2 = size / 2 - cut1;
+        if (n1 == 0) {
+            return ((double) nums2[(n2 - 1) / 2] + (double) nums2[n2 / 2]) / 2;
+        }
+        int len = n1 + n2;
+        int cutL = 0, cutR = n1;
+        int cut1 = n1 >> 1;
+        int cut2;
 
-        while (cut1 <= N1) {
-            cut1 = (cutR - cutL) / 2 + cutL;
-            cut2 = size / 2 - cut1;
-
-            double L1 = (cut1 == 0) ? MIN_VALUE : nums1[cut1 - 1];
-            double L2 = (cut2 == 0) ? MIN_VALUE : nums2[cut2 - 1];
-            double R1 = (cut1 == N1) ? MAX_VALUE : nums1[cut1];
-            double R2 = (cut2 == N2) ? MAX_VALUE : nums2[cut2];
-            if (L1 > R2)
+        while (cut1 <= n1) {
+            cut1 = ((cutR - cutL) >> 1) + cutL;
+            cut2 = (len >> 1) - cut1;
+            double l1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            double l2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            double r1 = (cut1 == n1) ? Integer.MAX_VALUE : nums1[cut1];
+            double r2 = (cut2 == n2) ? Integer.MAX_VALUE : nums2[cut2];
+            if (l1 > r2)
                 cutR = cut1 - 1;
-            else if (L2 > R1)
+            else if (l2 > r1)
                 cutL = cut1 + 1;
             else {// Otherwise, that's the right cut.
-                if (size % 2 == 0) {// 偶数个数的时候
-                    L1 = (L1 > L2 ? L1 : L2);
-                    R1 = (R1 < R2 ? R1 : R2);
-                    return (L1 + R1) / 2;
-                } else {
-                    R1 = (R1 < R2 ? R1 : R2);
-                    return R1;
+                if (len % 2 == 0) {// 偶数个数的时候
+                    l1 = Math.max(l1, l2);
+                    r1 = Math.min(r1, r2);
+                    return (l1 + r1) / 2;
                 }
+                // 奇数
+                r1 = Math.min(r1, r2);
+                return r1;
+            }
+        }
+        return -1;
+
+    }
+
+    public double findMedianSortedArrays4(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) {
+            return findMedianSortedArrays4(nums2, nums1);
+        }
+        if (n1 == 0) {
+            return ((double) nums2[(n2 - 1) >> 1] + (double) nums2[n2 >> 1]) / 2;
+        }
+        int len = n1 + n2;
+        int cutL = 0;
+        int cutR = n1;
+        int cut1 = n1 >> 1;
+        int cut2;
+        while (cut1 <= n1) {
+            cut1 = ((cutR - cutL) >> 1) + cutL;
+            cut2 = (len >> 1) - cut1;
+
+            double l1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            double l2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            double r1 = cut1 == n1 ? Integer.MAX_VALUE : nums1[cut1];
+            double r2 = cut2 == n2 ? Integer.MAX_VALUE : nums2[cut2];
+            if (l1 > r2) {
+                cutR = cut1 - 1;
+            } else if (l2 > r1) {
+                cutL = cut1 + 1;
+            } else {
+                // 偶数
+                if ((len & 1) == 0) {
+                    l1 = Math.max(l1, l2);
+                    r1 = Math.min(r1, r2);
+                    return (l1 + r1) / 2;
+                }
+                // 奇数
+                r1 = Math.min(r1, r2);
+                return r1;
             }
         }
         return -1;
     }
+
+    public double findMedianSortedArrays5(int[] nums1, int[] nums2) {
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        if (n1 > n2) {
+            return findMedianSortedArrays5(nums2, nums1);
+        }
+
+        if (n1 == 0) {
+            return ((double) nums2[(n2 - 1) >> 1] + (double) nums2[n2 >> 1]) / 2;
+        }
+        int len = n1 + n2;
+        int cutL = 0;
+        int cutR = n1;
+        int cut1 = n1 >> 1;
+        int cut2;
+        while (cut1 <= n1) {
+            cut1 = ((cutR - cutL) >> 1) + cutL;
+            cut2 = (len >> 1) - cut1;
+
+            double l1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            double l2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            double r1 = cut1 == n1 ? Integer.MAX_VALUE : nums1[cut1];
+            double r2 = cut2 == n2 ? Integer.MAX_VALUE : nums2[cut2];
+            if (l1 > r2) {
+                cutR = cut1 - 1;
+            } else if (l2 > r1) {
+                cutL = cut1 + 1;
+            } else {
+                // 偶数
+                if ((len & 1) == 0) {
+                    l1 = Math.max(l1, l2);
+                    r1 = Math.min(r1, r2);
+                    return (l1 + r1) / 2;
+                }
+                r1 = Math.min(r1, r2);
+                return r1;
+            }
+        }
+
+        return -1;
+
+
+    }
+
 
 }
