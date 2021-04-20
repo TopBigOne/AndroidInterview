d89bb46846d811d8254208407d69aad2d1625df2
Big North <chinagyzhou@gmail.com>
2021-04-20 20:24:55 +0800

    1:寻找两个正序数组的中位数 (字节半年20次)

---
+++ Algorithm/src/main/java/array/leet_code_4/FindMedianSortedArray.java
@@ -0,0 +1,154 @@
+package array.leet_code_4;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/4/20 10:19
+ * @Desc :
+ * <p>
+ * 题解：https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/
+ *
+ * https://blog.csdn.net/chen_xinjia/article/details/69258706
+ */
+public class FindMedianSortedArray {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) {
+            // 保证 m<=n
+            return findMedianSortedArrays(B, A);
+        }
+        int iMin = 0;
+        int iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) >> 1;
+            int j = ((m + n + 1) >> 1) - i;
+            // i 需要增大
+            if (j != 0 && i != m && B[j - 1] > A[i]) {
+                iMin = i + 1;
+            } else if (i != 0 && j != n && A[i - 1] > B[j]) {
+                // i 需要减小
+                iMax = i - 1;
+            } else {
+                // 达到要求，并且将边界条件列出来 单独考虑
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                // 奇数的话不需要考虑右半部分
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+
+    }
+
+    public double findMedianSortedArrays2(int[] A, int[] B) {
+        int m = A.length;
+        int n = B.length;
+        if (m > n) {
+            // 保证 m <= n
+            return findMedianSortedArrays(B, A);
+        }
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            // i 需要增大
+            if (j != 0 && i != m && B[j - 1] > A[i]) {
+                iMin = i + 1;
+            } else if (i != 0 && j != n && A[i - 1] > B[j]) {
+                // i 需要减小
+                iMax = i - 1;
+            } else {
+                // 达到要求，并且将边界条件列出来单独考虑
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = B[j - 1];
+                } else if (j == 0) {
+                    maxLeft = A[i - 1];
+                } else {
+                    maxLeft = Math.max(A[i - 1], B[j - 1]);
+                }
+                // 奇数的话不需要考虑右半部分
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                }
+
+                int minRight = 0;
+                if (i == m) {
+                    minRight = B[j];
+                } else if (j == n) {
+                    minRight = A[i];
+                } else {
+                    minRight = Math.min(B[j], A[i]);
+                }
+                //如果是偶数的话返回结果
+                return (maxLeft + minRight) / 2.0;
+            }
+        }
+        return 0.0;
+    }
+
+    public static double findMedianSortedArrays3(int[] nums1, int[] nums2) {
+        int MIN_VALUE = 0x80000000;
+
+        int MAX_VALUE = 0x7fffffff;
+
+        int N1 = nums1.length;
+        int N2 = nums2.length;
+        if (N1 > N2) {// 确保N1是短的部分。
+            return findMedianSortedArrays3(nums2, nums1);
+        }
+
+        if (N1 == 0)
+            return ((double) nums2[(N2 - 1) / 2] + (double) nums2[N2 / 2]) / 2;
+        int size = N1 + N2;
+        int cutL = 0, cutR = N1;
+        int cut1 = N1 / 2;
+        int cut2 = size / 2 - cut1;
+
+        while (cut1 <= N1) {
+            cut1 = (cutR - cutL) / 2 + cutL;
+            cut2 = size / 2 - cut1;
+
+            double L1 = (cut1 == 0) ? MIN_VALUE : nums1[cut1 - 1];
+            double L2 = (cut2 == 0) ? MIN_VALUE : nums2[cut2 - 1];
+            double R1 = (cut1 == N1) ? MAX_VALUE : nums1[cut1];
+            double R2 = (cut2 == N2) ? MAX_VALUE : nums2[cut2];
+            if (L1 > R2)
+                cutR = cut1 - 1;
+            else if (L2 > R1)
+                cutL = cut1 + 1;
+            else {// Otherwise, that's the right cut.
+                if (size % 2 == 0) {// 偶数个数的时候
+                    L1 = (L1 > L2 ? L1 : L2);
+                    R1 = (R1 < R2 ? R1 : R2);
+                    return (L1 + R1) / 2;
+                } else {
+                    R1 = (R1 < R2 ? R1 : R2);
+                    return R1;
+                }
+            }
+        }
+        return -1;
+    }
+
+}
