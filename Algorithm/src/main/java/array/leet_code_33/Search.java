204c8de54ddd29f72c414013c309bae869b51520
Big North <chinagyzhou@gmail.com>
2021-04-28 00:57:47 +0800

    33 搜索旋转排序数组;

---
+++ Algorithm/src/main/java/array/leet_code_33/Search.java
@@ -0,0 +1,244 @@
+package array.leet_code_33;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  4/27/21 10:06 PM
+ * @Desc : 33 搜索旋转排序数组：https://leetcode-cn.com/problems/search-in-rotated-sorted-array/submissions/
+ * <p>
+ * 二分查找的变型，考察一下二分查找
+ * <p>
+ * 题解：https://leetcode-cn.com/problems/search-in-rotated-sorted-array/solution/duo-si-lu-wan-quan-gong-lue-bi-xu-miao-dong-by-swe/
+ * <p>
+ * 可以再看看：153，1095，852；
+ */
+public class Search {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public int search2(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid;
+
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (target == nums[mid]) {
+                return mid;
+            }
+            // 先根据 nums[mid] 与 nums[low] 的关系，判断 mid 是在左段还在右段
+            if (nums[mid] >= nums[low]) {
+                //再判断 target 是在 mid 的左边还是在右边，从而调整左右边界 low 和 high
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public int search3(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+        return -1;
+
+    }
+
+    public int search4(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+
+            }
+        }
+
+        return -1;
+
+
+    }
+
+    public int search5(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+
+            if (nums[mid] >= nums[low]) {
+                if (target > nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+
+
+    }
+
+    public int search6(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+
+            } else {
+                if (target > nums[mid] && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+        }
+
+        return -1;
+
+
+    }
+
+    public int search7(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[mid] >= nums[low]) {
+                if (target >= nums[low] && target < nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid - 1;
+                }
+            } else {
+                if (target > nums[mid] && target >= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+
+
+    }
+
+    public int search8(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        int mid = 0;
+        while (low <= high) {
+            mid = low + ((high - low) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+
+        }
+
+        return -1;
+
+
+    }
+}
