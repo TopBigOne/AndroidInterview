ed8475848ca7eb44ea9e7bbcd8c9382f2a301603
Big North <chinagyzhou@gmail.com>
2021-05-07 00:42:04 +0800

    1：三数之和；

--- Algorithm/src/main/java/array/leet_code_15/ThreeSum.java
+++ Algorithm/src/main/java/array/leet_code_15/ThreeSum.java
@@ -3,37 +3,91 @@ package array.leet_code_15;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @author : dev
  * @version :
  * @Date :  4/27/21 9:37 PM
- * @Desc : 15:三数之和
+ * @Desc : 15:三数之和:字节：50 次
  */
 public class ThreeSum {
-    public  List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList();
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (nums == null) {
+            return ans;
+        }
+
         int len = nums.length;
-        if(nums == null || len < 3) return ans;
+
+        if (len < 3) return ans;
+
         Arrays.sort(nums); // 排序
-        for (int i = 0; i < len ; i++) {
-            if(nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
-            if(i > 0 && nums[i] == nums[i-1]) continue; // 去重
-            int L = i+1;
-            int R = len-1;
-            while(L < R){
+
+        for (int i = 0; i < len; i++) {
+            if (nums[i] > 0) break; // 如果当前数字大于0，则三数之和一定大于0，所以结束循环
+
+            if (i > 0 && nums[i] == nums[i - 1]) continue; // 去重
+
+            int L = i + 1;
+            int R = len - 1;
+            while (L < R) {
                 int sum = nums[i] + nums[L] + nums[R];
-                if(sum == 0){
-                    ans.add(Arrays.asList(nums[i],nums[L],nums[R]));
-                    while (L<R && nums[L] == nums[L+1]) L++; // 去重
-                    while (L<R && nums[R] == nums[R-1]) R--; // 去重
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[L], nums[R]));
+                    while (L < R && nums[L] == nums[L + 1]) L++; // 去重
+                    while (L < R && nums[R] == nums[R - 1]) R--; // 去重
                     L++;
                     R--;
-                }
-                else if (sum < 0) L++;
+                } else if (sum < 0) L++;
                 else if (sum > 0) R--;
             }
         }
         return ans;
+    }
+
+
+    public List<List<Integer>> threeSum2(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        int length;
+        if (nums == null || (length = nums.length) == 0) {
+            return result;
+        }
+        // 从小到大
+        Arrays.sort(nums);
+        for (int i = 0; i < length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int left = i + 1;
+            int right = length - 1;
+
+            while (left < right) {
+                if (nums[left] + nums[right] == target) {
+                    List<Integer> curr = new ArrayList<>();
+                    curr.add(nums[i]);
+                    curr.add(nums[left]);
+                    curr.add(nums[right]);
+                    result.add(curr);
+                    left++;
+                    right--;
+                    while (left < length && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+                    while (right > left && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (nums[left] + nums[right] > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+
+        }
+
+
+        return result;
     }
 }
