8d3b9d2d2935374dd26662356d975770e4fc850e
Big North <chinagyzhou@gmail.com>
2021-05-06 17:23:07 +0800

    测试查看看synchronzied 字节码文件

---
+++ java_thread/src/main/java/com/android/java_thread/synchronize/Demo_01.java
@@ -0,0 +1,17 @@
+package com.android.java_thread.synchronize;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  5/6/21 11:05 AM
+ * @Desc :
+ */
+public class Demo_01 {
+    private final Object lock = new Object();
+
+    public void test() {
+        synchronized (lock) {
+            // do sth....
+        }
+    }
+}
