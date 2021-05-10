5d7801b6f25d02b7f781ff63981f431f903ac1b2
Big North <chinagyzhou@gmail.com>
2021-05-10 21:34:02 +0800

    leetcoe :143:重排序链表

---
+++ Algorithm/src/main/java/linked_list/leet_143/ReorderList.java
@@ -0,0 +1,148 @@
+package linked_list.leet_143;
+
+import linked_list.ListNode;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/5/10 20:57
+ * @Desc : 字节半年：24次；143：重排链表 ：https://leetcode-cn.com/problems/reorder-list/
+ * 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+ * 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+ * <p>
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+ * 示例 2:
+ * <p>
+ * 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+ * 题解：https://leetcode-cn.com/problems/reorder-list/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-34/
+ */
+public class ReorderList {
+    private ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode tail = head;
+        head = head.next;
+
+        tail.next = null;
+
+        while (head != null) {
+            ListNode temp = head.next;
+            head.next = tail;
+            tail = head;
+            head = temp;
+        }
+
+        return tail;
+    }
+
+    private ListNode doReverse(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode temp = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = temp;
+        }
+        return pre;
+    }
+
+    /**
+     * 内部还包含了反转链表
+     *
+     * @param head
+     */
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null
+                || head.next.next == null) {
+            return;
+        }
+        //找中点，链表分成两个
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode newHead = slow.next;
+        slow.next = null;
+
+        //第二个链表倒置
+        newHead = reverseList(newHead);
+
+        //链表节点依次连接
+        while (newHead != null) {
+            ListNode temp = newHead.next;
+            newHead.next = head.next;
+            head.next = newHead;
+            head = newHead.next;
+            newHead = temp;
+        }
+
+    }
+
+    public void reorderList2(ListNode head) {
+        // corner case;
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
+        }
+        // 找重点，链表拆分整两个
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode newHead = slow.next;
+        slow.next = null;
+
+        // 反转第二个链表
+        newHead = doReverse(newHead);
+        slow.next = null;
+
+        while (newHead != null) {
+            ListNode temp = newHead.next;
+            newHead.next = head.next;
+            head.next = newHead;
+            head = newHead.next;
+            newHead = temp;
+        }
+    }
+
+    public void reorderList3(ListNode head){
+        if (head==null||head.next==null||head.next.next==null) {
+            return;
+        }
+        // 找中点，链表拆分成2个
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode newHead = slow.next;
+        newHead = doReverse(newHead);
+        while (newHead!=null){
+            ListNode temp = newHead.next;
+            newHead.next = head.next;
+            head.next = newHead;
+            head = head.next;
+            newHead = temp;
+        }
+
+    }
+
+
+
+
+}
