3321d27e7183cc61ab028dea1e61a762863cb199
Big North <chinagyzhou@gmail.com>
2021-05-10 20:53:34 +0800

    测试字节码 ：Ljava 是干嘛使的？

---
+++ java_base/src/main/java/com/android/base/bit_code/Demo.java
@@ -0,0 +1,19 @@
+package com.android.base.bit_code;
+
+import java.util.Arrays;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/4/30 19:34
+ * @Desc :
+ */
+public class Demo {
+    public static void main(String[] args) {
+        String [] strings = new String[10];
+        System.out.println(Arrays.toString(strings));
+        String str = "123";
+        System.out.println(str);
+
+    }
+}
