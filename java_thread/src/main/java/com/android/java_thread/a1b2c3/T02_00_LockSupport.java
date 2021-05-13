dc75a3668930e66999ba9e106e5d9cf7345c7be3
Big North <chinagyzhou@gmail.com>
2021-05-13 21:14:33 +0800

    1: 线程交替打印；

--- java_thread/src/main/java/com/android/java_thread/a1b2c3/T02_00_LockSupport.java
+++ java_thread/src/main/java/com/android/java_thread/a1b2c3/T02_00_LockSupport.java
@@ -9,13 +9,47 @@ import java.util.concurrent.locks.LockSupport;
  * @Desc : LockSupport park 当前线程阻塞（停止）
  */
 public class T02_00_LockSupport {
-    static Thread t1 = null, t2 = null;
+
 
     public static void main(String[] args) {
+        T02_00_LockSupport lockSupport = new T02_00_LockSupport();
+        // lockSupport.test1();
+        lockSupport.test2();
+    }
 
+    private void test1() {
+
+        // Thread t1 = null, t2 = null;
         char[] aI = "1234567".toCharArray();
         char[] aC = "ABCDEFG".toCharArray();
+        t1 = new Thread(() -> {
+            for (char c : aI) {
+                System.out.print(c);
+                LockSupport.unpark(t2);
+                LockSupport.park();
+            }
+        }, "t1");
 
+        t2 = new Thread(() -> {
+            for (char c : aC) {
+                LockSupport.park();
+                System.out.print(c);
+                LockSupport.unpark(t1);
+            }
+        }, "t2");
+
+        t1.start();
+        t2.start();
+    }
+
+    private Thread t1;
+    private Thread t2;
+    char[] aI = "1234567".toCharArray();
+    char[] aC = "ABCDEFG".toCharArray();
+
+
+    public void test2() {
+        System.out.println("↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓");
         t1 = new Thread(() -> {
             for (char c : aI) {
                 System.out.print(c);
@@ -33,9 +67,14 @@ public class T02_00_LockSupport {
             }
         }, "t2");
 
-
         t1.start();
         t2.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑↑");
 
     }
 
