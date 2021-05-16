13df03163b5f8e2a248fdc65e60805ea8fcbfd39
Big North <chinagyzhou@gmail.com>
2021-05-16 12:56:47 +0800

    leetcode 02 : 两数之和：https://leetcode-cn.com/problems/add-two-numbers/

--- Algorithm/src/main/java/linked_list/leet_code_2/AddTwoNumber.java
+++ Algorithm/src/main/java/linked_list/leet_code_2/AddTwoNumber.java
@@ -4,13 +4,15 @@ import linked_list.ListNode;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Currency;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * @author : dev
  * @version :
  * @Date :  2021/3/23 18:49
- * @Desc : 两数相加（https://leetcode-cn.com/problems/add-two-numbers/）
+ * @Desc : 两数相加 字节：60 次（https://leetcode-cn.com/problems/add-two-numbers/）
  * 链表解法（https://leetcode-cn.com/problems/add-two-numbers/solution/hua-jie-suan-fa-2-liang-shu-xiang-jia-by-guanpengc/）
  * <p>
  * https://www.youtube.com/watch?v=wDf2wcyP1iU(山景城一姐)
@@ -113,7 +115,6 @@ public class AddTwoNumber {
             if (p1 != null) {
                 p1 = p1.next;
             }
-
         }
         if (carry > 0) {
             cur.next = new ListNode(carry);
@@ -121,21 +122,70 @@ public class AddTwoNumber {
         return pre.next;
     }
 
+    public ListNode addTwoNumber4(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        ListNode cur = dummy;
 
-    public static void main(String[] args) {
-        List<Integer> d = new ArrayList<>();
-        List<Integer> p = d;
+        int carry = 0;
+        while (p1 != null || p2 != null) {
+            int x = p1 == null ? 0 : p1.val;
+            int y = p2 == null ? 0 : p2.val;
 
-        p.add(1);
-        p.add(3);
-        p.add(5);
-        System.out.println("TTTTTTT:p:" + Arrays.toString(p.toArray()));
-        d.remove(0);
+            int sum = carry + x + y;
+            carry = sum / 10;
+            sum %= 10;
 
-        System.out.println("TTTTTTT modify:d:" + Arrays.toString(d.toArray()));
+            cur.next = new ListNode(sum);
+            cur = cur.next;
 
-        System.out.println(5/10);
+            if (p1 != null) {
+                p1 = p1.next;
+            }
 
+            if (p2 != null) {
+                p2 = p2.next;
+            }
+            if (carry > 0) {
+                cur.next = new ListNode(carry);
+            }
+
+        }
+
+        return dummy.next;
+
+    }
+
+    public ListNode addTwoNumber5(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode pre = dummy;
+        ListNode p1 = l1;
+        ListNode p2 = l2;
+        int carry = 0;
+
+        while (p1 != null || p2 != null) {
+            int x = l1 == null ? 0 : l1.val;
+            int y = l2 == null ? 0 : l2.val;
+            int sum = carry + x + y;
+            carry = sum / 10;
+            pre.next = new ListNode(sum % 10);
+
+            pre = pre.next;
+
+            if (p1 != null) {
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                p2 = p2.next;
+            }
+
+        }
+        if (carry > 0) {
+            pre.next = new ListNode(carry);
+        }
+
+        return dummy.next;
 
     }
 
