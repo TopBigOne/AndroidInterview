2a52789e35187647a3db3be130af9eff4b827068
Big North <chinagyzhou@gmail.com>
2021-05-17 19:37:19 +0800

    hook handler 的callBack

---
+++ 02_handler/src/main/java/com/jar/yu/dialog/MockClass2.java
@@ -0,0 +1,26 @@
+package com.jar.yu.dialog;
+
+import android.os.Handler;
+import android.os.Message;
+
+import androidx.annotation.NonNull;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  5/17/21 1:23 PM
+ * @Desc :
+ */
+public class MockClass2  implements Handler.Callback {
+
+    /**
+     * Handler 的 call back
+     * @param msg
+     * @return
+     */
+    @Override
+    public boolean handleMessage(@NonNull Message msg) {
+
+        return false;
+    }
+}
