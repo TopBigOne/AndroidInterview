61391a69edbdb60beec0e6fa642fae7eb551f777
Big North <chinagyzhou@gmail.com>
2021-05-21 20:47:18 +0800

    字节面试题： 移除重复节点 ：https://leetcode-cn.com/problems/remove-duplicate-node-lcci/

---
+++ Algorithm/src/main/java/linked_list/interview_02_01/RemoveDuplicateNode.java
@@ -0,0 +1,42 @@
+package linked_list.interview_02_01;
+
+import linked_list.ListNode;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/5/21 19:46
+ * @Desc : 字节面试题： 移除重复节点 ：https://leetcode-cn.com/problems/remove-duplicate-node-lcci/
+ * <p>
+ * <p>
+ * 编写代码，移除未排序链表中的重复节点。保留最开始出现的节点。
+ * <p>
+ * 示例1:
+ * <p>
+ * 输入：[1, 2, 3, 3, 2, 1]
+ * 输出：[1, 2, 3]
+ * 示例2:
+ * <p>
+ * 输入：[1, 1, 1, 1, 2]
+ * 输出：[1, 2]
+ */
+public class RemoveDuplicateNode {
+    public ListNode removeDuplicateNodes(ListNode head) {
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode temp = cur;
+            while (temp.next != null) {
+                if (temp.next.val == cur.val) {
+                    temp.next = temp.next.next;
+                } else {
+                    temp = temp.next;
+                }
+            }
+            cur = cur.next;
+        }
+        return head;
+
+
+    }
+
+}
