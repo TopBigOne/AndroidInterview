f330d59978e2b9756d217a03a89d6702effa84d7
Big North <chinagyzhou@gmail.com>
2021-05-26 22:01:56 +0800

    leet code  46: 全排列：https://leetcode-cn.com/problems/permutations/

---
+++ Algorithm/src/main/java/back_tracking/leet_code_46/Permute.java
@@ -0,0 +1,13 @@
+package back_tracking.leet_code_46;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2021/5/26 21:55
+ * @Desc : leet code  46: 全排列：https://leetcode-cn.com/problems/permutations/
+ * 回溯算法入门；
+ *  题解：https://leetcode-cn.com/problems/permutations/solution/hui-su-suan-fa-python-dai-ma-java-dai-ma-by-liweiw/
+ *
+ */
+public class Permute {
+}
