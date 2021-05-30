5f5d789d675be3b687aa64dba68640d7d435821d
Big North <chinagyzhou@gmail.com>
2021-05-31 00:56:12 +0800

    leetcode 09: 回文数；字节 8 次：https://leetcode-cn.com/problems/palindrome-number/

---
+++ Algorithm/src/main/java/math/leet_code_09/IsPalindrome.java
@@ -0,0 +1,29 @@
+package math.leet_code_09;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  5/30/21 5:15 PM
+ * @Desc : leetcode 09: 回文数；字节 8 次：https://leetcode-cn.com/problems/palindrome-number/
+ * 快慢指针；
+ */
+public class IsPalindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        String newStr = String.valueOf(x);
+        int start = 0;
+        int end = newStr.length() - 1;
+        while (start < end) {
+            if (newStr.charAt(start) != newStr.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+
+    }
+
+}
