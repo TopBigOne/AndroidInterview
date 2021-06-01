d5d46a38ed1e3e640a98c646b2c181a8725334d1
Big North <chinagyzhou@gmail.com>
2021-06-01 19:55:40 +0800

    1: 重新回顾二分查找；

--- Algorithm/src/main/java/ten_sort/search/BinarySearch.java
+++ Algorithm/src/main/java/ten_sort/search/BinarySearch.java
@@ -13,175 +13,26 @@ public class BinarySearch {
         int[] rawData = {12, 4, 6, 2, 7, 1, 3, 8};
         Arrays.sort(rawData);
         System.out.println(" after sort arr:" + Arrays.toString(rawData));
-        // int index = getIndex4(12, rawData);
-        int index = getIndex6(rawData, 12);
+        BinarySearch binarySearch = new BinarySearch();
 
-        System.out.println("index:" + index);
+        int result = binarySearch.search(rawData, 12);
+
+        System.out.println("result:" + result);
     }
-
-    /**
-     * @param key
-     * @param rawData 数组必须是有序的。
-     * @return
-     */
-    private static int rank(int key, int[] rawData) {
-        int lo = 0;
-        int hi = rawData.length - 1;
-        while (lo <= hi) {
-            //  被查找的键要么不存在，要么必然存在于 a[o...hi] 之中；
-            int mid = lo + (hi - lo) >> 1;
-
-            // 与中间的那个数比较
-            if (key < rawData[mid]) {
-                // 左边找
-                hi = mid - 1;
-            } else if (key > rawData[mid]) {
-                // 右边找
-                lo = mid + 1;
-            } else {
+    public int search(int [] nums,int target){
+        int low = 0;
+        int high = nums.length;
+        while (low<=high){
+            int mid = low+((high-low)>>1);
+            int currentValue = nums[mid];
+            if(currentValue==target){
                 return mid;
+            }else if(currentValue<nums[mid]){
+                low =mid+1;
             }
-        }
-
-        return -1;
-    }
-
-
-    private static int getTargetIndex(int target, int[] rawData) {
-        int lo = 0;
-        int hi = rawData.length - 1;
-        while (lo <= hi) {
-            //  每次取中间值
-            int mid = lo + (hi - lo) / 2;
-            if (target < rawData[mid]) {
-                hi = mid - 1;
-            } else if (target > rawData[mid]) {
-                lo = mid + 1;
-            } else {
-                return mid;
+            else if(currentValue>target){
+                high = mid-1;
             }
-        }
-
-        return -1;
-    }
-
-
-    private static int getIndex2(int target, int[] rawData) {
-        int low = 0;
-        int high = rawData.length - 1;
-        while (low <= high) {
-            int mid = low + (high - low) >> 1;
-            if (target < rawData[mid]) high = mid - 1;
-            else if (target > rawData[mid]) low = mid + 1;
-            else return mid;
-        }
-        return -1;
-        //
-    }
-
-    private static int getIndex3(int target, int[] rawData) {
-        int low = 0;
-        int high = rawData.length - 1;
-        while (low <= high) {
-            int mid = low + ((high - low) >> 1);
-            if (target < rawData[mid]) high = mid - 1;
-            else if (target > rawData[mid]) low = mid + 1;
-            else return mid;
-        }
-        return -1;
-    }
-
-
-    private static int getIndex4(int target, int[] rawData) {
-        int low = 0;
-        int high = rawData.length - 1;
-        while (low <= high) {
-            int mid = low + ((high - low) >> 1);
-            if (target <= rawData[mid]) high = mid - 1;
-            if (target > rawData[mid]) low = mid + 1;
-            else return mid;
-        }
-        return -1;
-    }
-
-    private static int getIndex5(int target, int[] rawData) {
-        int low = 0;
-        int high = rawData.length - 1;
-        while (low <= high) {
-            int mid = low + ((high - low) >> 1);
-            if (target <= rawData[mid]) {
-                high = mid - 1;
-            } else if (target > rawData[mid]) {
-                low = mid + 1;
-            } else return mid;
-        }
-
-        return -1;
-    }
-
-
-    public static int getIndex6(int[] raw, int target) {
-        int low = 0;
-        int high = raw.length - 1;
-        while (low < high) {
-            int mid = low + (low + high) >> 1;
-            if (target > raw[mid]) high = mid + 1;
-            else if (target <= raw[mid]) low = mid - 1;
-            else return mid;
-        }
-        return -1;
-    }
-
-    public static int getIndex7(int[] raw, int target) {
-        int low = 0;
-        int high = raw.length - 1;
-        while (low < high) {
-            int mid = low + (low + high) >> 1;
-            // 在右侧
-            if (target > raw[mid]) {
-                high = mid + 1;
-            } else if (target <= raw[mid]) {
-                // 在左侧
-                low = mid - 1;
-
-            } else {
-                return mid;
-            }
-        }
-
-        return -1;
-    }
-
-    public static int getIndex8(int[] raw, int target) {
-        int low = 0;
-        int high = raw.length - 1;
-        while (low < high) {
-            // 位运算的优先级，是低于加减运算的
-            int mid = low + (high - low >> 1);
-            if (target > raw[mid]) {
-                high = mid + 1;
-            } else if (target < raw[mid]) {
-                low = mid - 1;
-            } else {
-                return mid;
-            }
-        }
-        return -1;
-    }
-
-    public static int getIndex9(int[] raw, int target) {
-        int low = 0;
-        int high = raw.length - 1;
-        while (low < high) {
-            int mid = low + (high - low >> 1);
-            if (target > raw[mid]) {
-                high = mid + 1;
-            } else if (target < raw[mid]) {
-                low = mid - 1;
-            } else {
-                return mid;
-            }
-
         }
         return -1;
     }
