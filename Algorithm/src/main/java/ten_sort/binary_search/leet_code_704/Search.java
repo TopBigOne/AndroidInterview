bfd46735a2bca4eaf8a01cff2b1256e6bf6bc6dc
Big North <chinagyzhou@gmail.com>
2021-06-02 00:37:05 +0800

    二分查找；

--- Algorithm/src/main/java/ten_sort/binary_search/leet_code_704/Search.java
+++ Algorithm/src/main/java/ten_sort/binary_search/leet_code_704/Search.java
@@ -8,26 +8,30 @@ package ten_sort.binary_search.leet_code_704;
  * @Level : easy medium hard
  * @Desc :  二分查找
  * @Counter : 9
- * @Answer :
+ * @Answer : https://leetcode-cn.com/problems/binary-search/solution/er-fen-cha-zhao-xiang-jie-by-labuladong/
  */
 public class Search {
     public int search(int[] nums, int target) {
         int low = 0;
+        // 1: 注意 数组是从 0 开始的；
         int high = nums.length - 1;
-        while (low < high) {
+        while (low <=high) {
+            // 2： >> 和 + ,优先级问题；
             int mid = low + (high - low) >> 1;
-            if (nums[mid] == target) {
+            int currValue = nums[mid];
+            // 3-1： 恰巧相等；
+            if (currValue == target) {
                 return mid;
-            } else if (target>nums[mid]) {
-                high = mid - 1;
-
-            } else {
+            } else if (currValue < target) {
+                // 3-2：当前下标所对应的值，比target 要小，低位，要向右边走；
                 low = mid + 1;
+            } else {
+                // 3-3： 当前小标所对应的值，比 target 要打，这时；高位，向左边走；
+                high = mid - 1;
             }
         }
-
+        // 4： 没有找到合适的值；
         return -1;
-
     }
 
 }
