7cc1a10ed05bfc401096f427ffeb6418ee04f46c
Big North <chinagyzhou@gmail.com>
2021-06-04 19:31:10 +0800

    简化节点实体类；

--- Algorithm/src/main/java/linked_list/ListNode.java
+++ Algorithm/src/main/java/linked_list/ListNode.java
@@ -24,7 +24,7 @@ public class ListNode {
 
     @Override
     public String toString() {
-        return "ListNode{" +
+        return "{" +
                 "val = " + val +
                 '}';
     }
