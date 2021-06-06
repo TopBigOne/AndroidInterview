1731bef85acc32ce44b6c938b226c41b7c7f5b83
Big North <chinagyzhou@gmail.com>
2021-06-06 17:18:31 +0800

    快速排序：基本写法

---
+++ Algorithm/src/main/java/ten_sort/quick_sort/QuickSort_Base.java
@@ -0,0 +1,78 @@
+package ten_sort.quick_sort;
+
+import java.util.Random;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  6/5/21 4:38 PM
+ * @Desc : 快速排序：基本写法
+ * 题解：https://leetcode-cn.com/problems/sort-an-array/solution/fu-xi-ji-chu-pai-xu-suan-fa-java-by-liweiwei1419/
+ */
+public class QuickSort_Base {
+    public static final int INSERTION_SORT_THRESHOLD = 7;
+    private static final Random RANDOM = new Random();
+
+    public int[] sortArray(int[] nums) {
+        int len = nums.length;
+        quickSort(nums, 0, len - 1);
+        return nums;
+    }
+
+    private void quickSort(int[] nums, int left, int right) {
+        // 小区间使用插入排序
+        if (right - left <= INSERTION_SORT_THRESHOLD) {
+            insertionSort(nums, left, right);
+            return;
+        }
+        int pIndex = partition(nums, left, right);
+        quickSort(nums, left, pIndex - 1);
+        quickSort(nums, pIndex + 1, right);
+    }
+
+    /**
+     * 对 数组的子区间[left,right] 使用插入排序
+     *
+     * @param nums
+     * @param left
+     * @param right
+     */
+    private void insertionSort(int[] nums, int left, int right) {
+        for (int i = left + 1; i <= right; i++) {
+            int temp = nums[i];
+            int j = i;
+            while (j > left && nums[j - 1] > temp) {
+                nums[j] = nums[j - 1];
+                j--;
+            }
+            nums[j] = temp;
+        }
+    }
+
+    private int partition(int[] nums, int left, int right) {
+        int randomIndex = RANDOM.nextInt(right - left + 1) + left;
+        swap(nums, left, randomIndex);
+        // 基准值
+        int pivot = nums[left];
+        int lt = left;
+        // 循环不变量，
+        // all in [left+1,lt] < pivot;
+        // all in [lt +1,i] >= pivot;
+        for (int i = left + 1; i <= right; i++) {
+            if (nums[i] < pivot) {
+                lt++;
+                swap(nums, i, lt);
+            }
+        }
+
+        swap(nums, left, lt);
+        return lt;
+    }
+
+    private void swap(int[] nums, int index1, int index2) {
+        int temp = nums[index1];
+        nums[index1] = nums[index2];
+        nums[index2] = temp;
+    }
+
+}
