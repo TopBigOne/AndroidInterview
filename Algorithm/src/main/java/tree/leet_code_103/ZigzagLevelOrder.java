a6956df1225369421a2eca1ba0e03ff13a6a0ecf
Big North <chinagyzhou@gmail.com>
2021-06-16 19:29:14 +0800

    leet_code_103 二叉树的锯齿形层序遍历 https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/

--- Algorithm/src/main/java/tree/leet_code_103/ZigzagLevelOrder.java
+++ Algorithm/src/main/java/tree/leet_code_103/ZigzagLevelOrder.java
@@ -2,18 +2,64 @@ package tree.leet_code_103;
 
 import tree.TreeNode;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
+
+
 /**
- * @author : dev
+ * @author :  dev
  * @version :
- * @Date :  2021/5/28 17:36
- * @Desc : https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
- * 搜狐一面
+ * @Date :   2021/5/28 17:36
+ * @Url :  https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/
+ * @Level : easy medium hard
+ * @Desc :  二叉树的锯齿形层序遍历
+ * @Counter :31
+ * @Answer : https://leetcode-cn.com/problems/binary-tree-zigzag-level-order-traversal/solution/bfshe-dfsliang-chong-jie-jue-fang-shi-by-184y/
  */
 public class ZigzagLevelOrder {
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        if (root==null) {
+            return result;
+        }
+
+        LinkedList<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int order = 1;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> temp = new ArrayList<>();
+            TreeNode node;
+            for (int i = 0; i < size; i++) {
+                node = queue.pollFirst();
+                if (node == null) {
+                    continue;
+                }
+
+                temp.add(node.val);
+
+                TreeNode l = node.left;
+                TreeNode r = node.right;
+                if (l != null) {
+                    queue.offer(l);
+                }
+                if (r != null) {
+                    queue.offer(r);
+                }
+            }
+
+            if ((order & 1) == 0) {
+                Collections.reverse(temp);
+            }
+            order++;
+            result.add(temp);
+        }
+
+        return result;
 
     }
+
 }
