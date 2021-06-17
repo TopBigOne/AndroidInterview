54d25c6cabba1f5f158266b76f83723266c19aab
Big North <chinagyzhou@gmail.com>
2021-06-18 01:38:35 +0800

    leetcode_39: 组合总和：https://leetcode-cn.com/problems/combination-sum/

---
+++ Algorithm/src/main/java/back_tracking/leet_code_39/CombinationSum.java
@@ -0,0 +1,55 @@
+package back_tracking.leet_code_39;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  6/18/21 12:45 AM
+ * @Desc : https://leetcode-cn.com/problems/combination-sum/
+ * 题解：https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
+ */
+public class CombinationSum {
+    public static void main(String[] args) {
+        CombinationSum combinationSum = new CombinationSum();
+        int[] candicates = new int[]{2, 3, 6, 7};
+        int target = 7;
+        List<List<Integer>> result = combinationSum.combinationSum(candicates, target);
+        System.out.println("result : " + result);
+    }
+
+    public List<List<Integer>> combinationSum( int[] candidates, int target) {
+        int len = candidates.length;
+        List<List<Integer>> res = new ArrayList<>();
+        if (len == 0) {
+            return res;
+        }
+        Deque<Integer> path = new ArrayDeque<>();
+        dfs(candidates, 0, len, target, path, res);
+        return res;
+    }
+
+    private void dfs(int[] candidates, int begin, int len, int target, Deque<Integer> path, List<List<Integer>> res) {
+        if (target < 0) {
+            return;
+        }
+        if (target == 0) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = begin; i < len; i++) {
+            path.addLast(candidates[i]);
+            System.out.println("递归之前 =>" + path + ", 剩余 = " + (target - candidates[i]));
+            dfs(candidates, i, len, target - candidates[i], path, res);
+            path.removeLast();
+            System.out.println("递归之后=> " + path);
+        }
+
+    }
+
+}
