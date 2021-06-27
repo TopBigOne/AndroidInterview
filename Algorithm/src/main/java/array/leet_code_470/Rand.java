5dd7160d19c550d266bed0f43bf1a74f4f7bfde8
Big North <chinagyzhou@gmail.com>
2021-06-28 01:55:29 +0800

    leetcode 470 用 Rand7() 实现 Rand10()

---
+++ Algorithm/src/main/java/array/leet_code_470/Rand.java
@@ -0,0 +1,35 @@
+package array.leet_code_470;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  6/27/21 11:44 PM
+ * @Desc : 用 Rand7() 实现 Rand10()
+ * 已有方法 rand7 可生成 1 到 7 范围内的均匀随机整数，试写一个方法 rand10 生成 1 到 10 范围内的均匀随机整数。
+ * <p>
+ * 不要使用系统的 Math.random() 方法。
+ * 题解： https://leetcode-cn.com/problems/implement-rand10-using-rand7/solution/xiang-xi-si-lu-ji-you-hua-si-lu-fen-xi-zhu-xing-ji/
+ */
+public class Rand extends SolBase {
+    public int rand10() {
+        while (true) {
+            int num = (rand7() - 1) * 7 + rand7();
+            // 如果在40以内，那就直接返回
+            if (num <= 40) {
+                return 1 + num % 10;
+            }
+            // 说明刚才生成的在41-49之间，利用随机数再操作一遍
+            num = (num - 40 - 1) * 7 + rand7();
+
+            if (num <= 60) {
+                return 1 + num % 10;
+            }
+            // 说明刚才生成的在61-63之间，利用随机数再操作一遍
+            num = (num - 60 - 1) * 7 + rand7();
+            if (num <= 20) {
+                return 1 + num % 10;
+            }
+        }
+
+    }
+}
---
+++ Algorithm/src/main/java/array/leet_code_470/SolBase.java
@@ -0,0 +1,14 @@
+package array.leet_code_470;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  6/27/21 11:44 PM
+ * @Desc :
+ */
+public class SolBase {
+    public int rand7(){
+
+        return 7;
+    }
+}
