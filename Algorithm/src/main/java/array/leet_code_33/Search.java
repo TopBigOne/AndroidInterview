d20fe4a026b1a93792e67028400dba492ee1562b
Big North <chinagyzhou@gmail.com>
2021-07-07 02:36:13 +0800

    33 搜索旋转排序数组：https://leetcode-cn.com/problems/search-in-rotated-sorted-array/submissions/

--- Algorithm/src/main/java/array/leet_code_33/Search.java
+++ Algorithm/src/main/java/array/leet_code_33/Search.java
@@ -283,7 +283,7 @@ public class Search {
         int high = nums.length - 1;
         int mid = 0;
         while (low <= high) {
-            mid  = low+((high-low)>>1);
+            mid = low + ((high - low) >> 1);
             if (nums[mid] == target) {
                 return mid;
             }
@@ -306,7 +306,114 @@ public class Search {
         }
         return -1;
 
+    }
 
+    /**
+     * nums = [4,5,6,7,0,1,2]
+     * 1:首先根据 nums[0] 与 target 的关系判断 target 是在左段还是在右段
+     * 2： 假如 target = 5 目标值在左半段，因此在[4, 5, 6, 7, inf, inf, inf] 这个有序数组里查找
+     * 3： 假如 target = 1, 目标值在右半段，因此在[-inf, -inf, -inf, -inf, 0, 1, 2]，这个有序数组里查找
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int search11(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            // 先根据 nums[0] 与 target 的关系判断目标值在左半段 还是在右半段
+            if (target >= nums[0]) {
+                // 左半段
+                // 目标值在左半段，若 mid 在 右半段，则将 mid 索引的值改为 inf；
+                if (nums[mid] < nums[0]) {
+                    nums[mid] = Integer.MAX_VALUE;
+                }
+
+            } else {
+                if (nums[mid] >= nums[0]) {
+                    nums[mid] = Integer.MIN_VALUE;
+                }
+            }
+
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+
+        return -1;
+    }
+
+    public int search12(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (target > nums[0]) {
+                // 目标值在左半段，但是 mid 值落在了右半段；
+                if (nums[mid] < nums[0]) {
+                    nums[mid] = Integer.MAX_VALUE;
+                }
+
+            } else {
+                // 目标值在右半段，但是 mid 留在了左半段
+                if (nums[mid] > nums[0]) {
+                    nums[mid] = Integer.MIN_VALUE;
+                }
+            }
+
+            if (nums[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+
+        }
+
+        return -1;
+
+    }
+
+    public int search13(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if(nums[mid]==target) return mid;
+            
+            if (target >= nums[0]) {
+                // 在左边
+                if (nums[mid] < nums[0]) {
+                    nums[mid] = Integer.MAX_VALUE;
+                }
+            } else {
+                if (nums[mid] >= nums[0]) {
+                    nums[mid] = Integer.MIN_VALUE;
+                }
+            }
+
+            if (nums[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid +1;
+            }
+
+        }
+
+        return -1;
     }
 
 
