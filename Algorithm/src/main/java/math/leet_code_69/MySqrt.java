f7757dee185347aa591ab28ac056e482b955a66b
Big North <chinagyzhou@gmail.com>
2021-07-12 00:38:41 +0800

    leetcode： 69； x 的平方根字 ；节半年24 次 https://leetcode-cn.com/problems/sqrtx/

--- Algorithm/src/main/java/math/leet_code_69/MySqrt.java
+++ Algorithm/src/main/java/math/leet_code_69/MySqrt.java
@@ -5,37 +5,26 @@ package math.leet_code_69;
  * @version :
  * @Date :  5/30/21 7:45 PM
  * @Desc :leetcode： 69； x 的平方根字 ；节半年24 次 https://leetcode-cn.com/problems/sqrtx/
- *
+ * <p>
  * 题解：https://leetcode-cn.com/problems/sqrtx/solution/niu-dun-die-dai-fa-by-loafer/
  * 牛顿迭代法
  */
 public class MySqrt {
     int s;
+
     public int mySqrt(int x) {
         s = x;
-        if (x == 0) {
-            return 0;
-        }
-        return (int) sqrt(x);
+        if (x == 0) return 0;
+        return (   (int) (sqrts(x))    );
     }
 
-    public double sqrt(double x) {
+    public double sqrts(double x) {
         double res = (x + s / x) / 2;
         if (res == x) {
             return x;
         } else {
-            return sqrt(res);
+            return sqrts(res);
         }
     }
-
-    public double sqrt2(double x){
-        double res = (x+s/x)/2;
-        if(res==x){
-            return x;
-        }
-        return sqrt(res);
-    }
-
-
 
 }
