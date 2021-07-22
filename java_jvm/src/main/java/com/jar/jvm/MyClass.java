31d830554dd48cae56c0b9d3cf4dc130b8521900
Big North <chinagyzhou@gmail.com>
2021-07-22 20:11:53 +0800

    1： 为了看Class 文件；

--- java_jvm/src/main/java/com/jar/jvm/MyClass.java
+++ java_jvm/src/main/java/com/jar/jvm/MyClass.java
@@ -1,7 +1,21 @@
 package com.jar.jvm;
 
 public class MyClass {
+
+    public static void main(String[] args) {
+        new MyClass();
+    }
+
+
     public MyClass() {
-        int a= 10;
+        int a = 0;
+        int b = 0;
+
+        int c = a++;
+        int d = ++b;
+
+        System.out.println("c : "+c);
+        System.out.println("d : "+d);
+
     }
 }
