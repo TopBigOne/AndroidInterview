4457642c278e0c5435c29a0f7e87ab6053b00068
guangya.zhou <chinagyzhou@gmail.com>
2022-01-10 23:41:25 +0800

    1：为了月薪4万，而努力着 不解释，不纠缠，不对抗！
    --------
    1: 需要好好针对大厂，改一下简历！

--- Algorithm/src/main/java/linked_list/leet_code_25/ReverseKGroup6.java
+++ Algorithm/src/main/java/linked_list/leet_code_25/ReverseKGroup6.java
@@ -6,8 +6,8 @@ import linked_list.ListNode;
  * @author :  dev
  * @version :
  * @Date :   2022/1/9 21:34
- * @Url :
- * @Level :  easy  medium hard
+ * @Url : https://leetcode-cn.com/problems/reverse-nodes-in-k-group/solution/
+ * @Level :   hard
  * @Desc :
  * @Counter :
  * @Answer :
@@ -37,9 +37,7 @@ public class ReverseKGroup6 extends BaseOperation {
             pre = start;
             end = start;
         }
-
         return dummy.next;
-
     }
 
     public ListNode reverseKGroup2(ListNode head, int k) {
@@ -66,9 +64,44 @@ public class ReverseKGroup6 extends BaseOperation {
             pre = start;
             end = start;
         }
-
         return dummy.next;
+    }
 
+    public ListNode reverseKGroup3(ListNode head, int k) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode dummy = new ListNode(-1);
+        dummy.next = dummy;
+        ListNode pre = dummy;
+        ListNode end = dummy;
+        while (end.next != null) {
+            for (int i = 0; i < k && end != null; i++) {
+                end = end.next;
+            }
+
+            if (end == null) {
+                break;
+            }
+//            ListNode temp = end.next;
+//            end.next = null;
+//            ListNode start = pre.next;
+//            pre.next = reverse(start);
+//            start.next = temp;
+//            pre = start;
+//            end = start;
+
+            ListNode temp = end.next;
+            end.next = null;
+
+            ListNode start = pre.next;
+            pre.next = reverse(start);
+            start.next = temp;
+            pre = start;
+            end = start;
+
+        }
+        return dummy.next;
 
     }
 }
