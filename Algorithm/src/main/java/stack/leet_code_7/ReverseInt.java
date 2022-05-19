eb5b1545138c214d2cbaeacd917861a707684582
guangya.zhou <chinagyzhou@gmail.com>
2022-05-19 17:23:22 +0800

    不解释，不纠缠，不对抗！  3小时力扣，3小时openGL ,3小时项目 -------- 1：整数反转；

--- Algorithm/src/main/java/stack/leet_code_7/ReverseInt.java
+++ Algorithm/src/main/java/stack/leet_code_7/ReverseInt.java
@@ -1,27 +1,53 @@
 package stack.leet_code_7;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.Stack;
-
 /**
  * @author : dev
  * @version :
  * @Date :  3/14/21 9:58 PM
- * @Desc :
+ * @Desc :   整数 反转：https://leetcode.cn/problems/reverse-integer/solution/
+ * <p>
+ * 题解：https://leetcode.cn/problems/reverse-integer/solution/hua-jie-suan-fa-7-zheng-shu-fan-zhuan-by-guanpengc/
  */
 public class ReverseInt {
-//    public int reverse(int x) {
-//        if (x == 0) {
-//            return 0;
-//        }
-//
-//
-//    }
+    public int reverse(int x) {
+        int res = 0;
+        while (x != 0) {
+            // 取余操作
+            int pop = x % 10;
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && pop > 7)) {
+                return 0;
+            }
 
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && pop < -8)) {
+                return 0;
+            }
+            res = res * 10 + pop;
+            //每次减少一个数量级（10）
+            x /= 10;
 
-    public static void main(String[] args) {
-        ReverseInt reverseInt = new ReverseInt();
-//        int result1 = reverseInt.reverse(9646324351);
+        }
+        return res;
+
     }
+
+    public int reverse2(int x) {
+        int res = 0;
+        while (x != 0) {
+
+            int carry = x % 10;
+            if (res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && carry > 7)) {
+                return 0;
+            }
+            if (res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && carry < -8)) {
+                return 0;
+            }
+            res = res * 10 + carry;
+            x /= 10;
+        }
+        return res;
+
+
+    }
+
+
 }
---
+++ Algorithm/src/main/java/stack/leet_code_7/Test.java
@@ -0,0 +1,15 @@
+package stack.leet_code_7;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2022/5/19 17:05
+ * @Desc :
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        ReverseInt reverseInt = new ReverseInt();
+//        int result1 = reverseInt.reverse(9646324351);
+    }
+}
