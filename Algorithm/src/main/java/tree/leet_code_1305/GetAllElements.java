db57725f537bde22c68431297e5367db978d6128
guangya.zhou <chinagyzhou@gmail.com>
2022-06-04 17:31:01 +0800

    不解释，不纠缠，不对抗！ ------ 1: 今天睡了很久！   二叉搜索树，中序遍历，获取一个有一个有序的列表！

---
+++ Algorithm/src/main/java/tree/leet_code_1305/GetAllElements.java
@@ -0,0 +1,55 @@
+package tree.leet_code_1305;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import tree.TreeNode;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2022/6/2 18:25
+ * @Desc :
+ */
+public class GetAllElements {
+
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> l1 = new ArrayList<>();
+        List<Integer> l2 = new ArrayList<>();
+        List<Integer> res = new ArrayList<>();
+
+
+        inorder(root1, l1);
+        inorder(root2, l2);
+
+        int m = l1.size();
+        int n = l2.size();
+        int i = 0;
+        int j = 0;
+        while (i < m && j < n) {
+            int temp = l1.get(i) < l2.get(j) ? l1.get(i++) : l2.get(j++);
+            res.add(temp);
+        }
+
+        if (i < m) {
+            res.addAll(l1.subList(i, m));
+        }
+        if (j < n) {
+            res.addAll(l2.subList(j, n));
+        }
+
+
+        return res;
+    }
+
+    private void inorder(TreeNode root, List<Integer> l1) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left, l1);
+        l1.add(root.val);
+        inorder(root.right, l1);
+    }
+
+
+}
