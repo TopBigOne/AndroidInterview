bf8ed7902482c0359bf65cd9e5220a6b101a1b79
guangya.zhou <chinagyzhou@gmail.com>
2022-06-26 22:03:43 +0800

    说话，不要激动...压住自己的那股劲！ NOTE：不和猪争吵； ------  leet code  884

--- Algorithm/src/main/java/string/leet_code_884/UncommonFromSentences2.java
+++ Algorithm/src/main/java/string/leet_code_884/UncommonFromSentences2.java
@@ -1,9 +1,6 @@
 package string.leet_code_884;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author :  dev
@@ -16,11 +13,18 @@ import java.util.Map;
  * @Answer : https://leetcode-cn.com/problems/uncommon-words-from-two-sentences/solution/bian-cheng-xiong-ha-xi-biao-acmjin-pai-t-ok6w/
  */
 public class UncommonFromSentences2 {
+    public static void main(String[] args) {
+        UncommonFromSentences2 uncommonFromSentences = new UncommonFromSentences2();
+        String s1 = "this apple is sweet", s2 = "this apple is sour";
+        String[] result = uncommonFromSentences.uncommonFromSentences(s1, s2);
+        System.err.println("result : " + Arrays.toString(result));
+    }
 
     public String[] uncommonFromSentences(String s1, String s2) {
         String[] arr1 = s1.split(" ");
         String[] arr2 = s2.split(" ");
 
+        // 单词，出现的数量
         Map<String, Integer> freq = new HashMap<>();
         for (String s : arr1) {
             freq.put(s, freq.getOrDefault(s, 0) + 1);
@@ -29,7 +33,9 @@ public class UncommonFromSentences2 {
         for (String s : arr2) {
             freq.put(s, freq.getOrDefault(s, 0) + 1);
         }
+
         List<String> res = new ArrayList<>();
+
         for (Map.Entry<String, Integer> entry : freq.entrySet()) {
             if (entry.getValue() == 1) {
                 res.add(entry.getKey());
