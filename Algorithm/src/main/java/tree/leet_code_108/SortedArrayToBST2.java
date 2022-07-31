f6546cd8be843441b9b46e05dfcc47995febe078
guangya.zhou <chinagyzhou@gmail.com>
2022-07-31 15:30:01 +0800

    * @Url : https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/  * @Level :  easy  * @Desc : 将有序数组转化为二叉搜索树

---
+++ Algorithm/src/main/java/tree/leet_code_108/SortedArrayToBST2.java
@@ -0,0 +1,27 @@
+package tree.leet_code_108;
+
+import tree.TreeNode;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2022/7/31 15:22
+ * @Desc :
+ */
+public class SortedArrayToBST2 {
+
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode dfs(int[] nums, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+        int mid = low + (high - low) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = dfs(nums, low, mid - 1);
+        root.right = dfs(nums, mid + 1, high);
+        return root;
+    }
+}
