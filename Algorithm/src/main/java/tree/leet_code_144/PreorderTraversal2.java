4f7b15ecdd1318acceec749f3bfa14bf4e9ca4e2
guangya.zhou <chinagyzhou@gmail.com>
2022-07-31 16:32:26 +0800

    二叉树的前序遍历

--- Algorithm/src/main/java/tree/leet_code_144/PreorderTraversal2.java
+++ Algorithm/src/main/java/tree/leet_code_144/PreorderTraversal2.java
@@ -1,11 +1,11 @@
 package tree.leet_code_144;
 
-import tree.TreeNode;
-
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
+
+import tree.TreeNode;
 
 /**
  * @author :  dev
@@ -15,22 +15,68 @@ import java.util.List;
  * @Level :  easy
  * @Desc :
  * @Counter :
- * @Answer :
+ * @Answer : https://leetcode.cn/problems/binary-tree-preorder-traversal/solution/leetcodesuan-fa-xiu-lian-dong-hua-yan-shi-xbian-2/
  */
 public class PreorderTraversal2 {
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        if (root==null) {
+        if (root == null) {
             return result;
         }
         Deque<TreeNode> stack = new ArrayDeque<>();
         stack.push(root);
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             TreeNode node = stack.pop();
             result.add(node.val);
-            if(node.right!=null) stack.push(node.right);
-            if(node.left!=null) stack.push(node.left);
+
+            if (node.right != null) stack.push(node.right);
+            if (node.left != null) stack.push(node.left);
         }
         return result;
+    }
+
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            result.add(node.val);
+
+            if (node.right != null) {
+                stack.push(node.right);
+            }
+            if (node.left != null) {
+                stack.push(node.left);
+
+            }
+
+        }
+        return result;
+    }
+
+    public List<Integer> preorderTraversal3(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode node = stack.pop();
+            res.add(node.val);
+            TreeNode r;
+            if ((r = node.right) != null) {
+                stack.push(r);
+            }
+            TreeNode l;
+            if ((l = node.left) != null) {
+                stack.push(l);
+            }
+        }
+
+        return res;
+
     }
 }
