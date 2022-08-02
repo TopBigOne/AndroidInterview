b05c592bc76503a4ad88705c787d3ef1eb48a297
guangya.zhou <chinagyzhou@gmail.com>
2022-08-02 11:38:53 +0800

    165. 比较版本号  https://leetcode-cn.com/problems/compare-version-numbers/

---
+++ Algorithm/src/main/java/string/leet_code_165/CompareVersion7.java
@@ -0,0 +1,41 @@
+package string.leet_code_165;
+
+/**
+ * @author : dev
+ * @version :
+ * @Date :  2022/8/2 11:15
+ * @Desc :  165. 比较版本号  https://leetcode-cn.com/problems/compare-version-numbers/
+ */
+public class CompareVersion7 {
+    public int compareVersion(String version1, String version2) {
+        int m = version1.length();
+        int n = version2.length();
+        int i = 0;
+        int j = 0;
+        while (i < m || j < n) {
+            int num1 = 0;
+            int num2 = 0;
+            while (i < m && version1.charAt(i) != '.') {
+                num1 = num1 * 10 + version1.charAt(i) - '0';
+                i++;
+            }
+
+            while (j < n && version2.charAt(j) != '.') {
+                num2 = num2 * 10 + version2.charAt(j) - '0';
+                j++;
+            }
+            if (num1 < num2) {
+                return -1;
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+            i++;
+            j++;
+        }
+
+        // final;
+        return 0;
+
+    }
+}
