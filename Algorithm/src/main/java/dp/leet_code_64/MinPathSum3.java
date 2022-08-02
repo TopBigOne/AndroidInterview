2b6a9977bdd366d227f008b2ad0dd03002c1d37c
guangya.zhou <chinagyzhou@gmail.com>
2022-08-02 13:34:42 +0800

    64. 最小路径和 https://leetcode-cn.com/problems/minimum-path-sum/submissions/

--- Algorithm/src/main/java/dp/leet_code_64/MinPathSum3.java
+++ Algorithm/src/main/java/dp/leet_code_64/MinPathSum3.java
@@ -1,16 +1,12 @@
 package dp.leet_code_64;
 
 /**
- * @author :  dev
+ * @author : dev
  * @version :
- * @Date :  2021/6/24 16:14
- * @Url :  64. 最小路径和 https://leetcode-cn.com/problems/minimum-path-sum/submissions/
- * @Level :  medium
- * @Desc : 64. 最小路径和
- * @Counter : 9
- * @Answer : https://leetcode-cn.com/problems/minimum-path-sum/solution/zui-xiao-lu-jing-he-dong-tai-gui-hua-gui-fan-liu-c/
+ * @Date :  2022/8/2 12:45
+ * @Desc :
  */
-public class MinPathSum {
+public class MinPathSum3 {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -28,9 +24,14 @@ public class MinPathSum {
                     grid[i][j] = grid[i - 1][j] + grid[i][j];
                     continue;
                 }
-                grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+                int left = grid[i - 1][j];
+                int top = grid[i][j - 1];
+
+                grid[i][j] = Math.min(left, top) + grid[i][j];
             }
         }
         return grid[m - 1][n - 1];
+
+
     }
 }
--- Algorithm/src/main/java/dp/leet_code_64/MinPathSum4.java
+++ Algorithm/src/main/java/dp/leet_code_64/MinPathSum4.java
@@ -1,16 +1,12 @@
 package dp.leet_code_64;
 
 /**
- * @author :  dev
+ * @author : dev
  * @version :
- * @Date :  2021/6/24 16:14
- * @Url :  64. 最小路径和 https://leetcode-cn.com/problems/minimum-path-sum/submissions/
- * @Level :  medium
- * @Desc : 64. 最小路径和
- * @Counter : 9
- * @Answer : https://leetcode-cn.com/problems/minimum-path-sum/solution/zui-xiao-lu-jing-he-dong-tai-gui-hua-gui-fan-liu-c/
+ * @Date :  2022/8/2 13:23
+ * @Desc :
  */
-public class MinPathSum {
+public class MinPathSum4 {
     public int minPathSum(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
@@ -20,17 +16,26 @@ public class MinPathSum {
                 if (i == 0 && j == 0) {
                     continue;
                 }
+
                 if (i == 0) {
                     grid[i][j] = grid[i][j - 1] + grid[i][j];
                     continue;
                 }
+
                 if (j == 0) {
                     grid[i][j] = grid[i - 1][j] + grid[i][j];
                     continue;
                 }
-                grid[i][j] = Math.min(grid[i - 1][j], grid[i][j - 1]) + grid[i][j];
+
+                int top = grid[i][j - 1];
+                int left = grid[i - 1][j];
+
+                grid[i][j] = Math.min(top, left) + grid[i][j];
             }
         }
+
         return grid[m - 1][n - 1];
+
+
     }
 }
