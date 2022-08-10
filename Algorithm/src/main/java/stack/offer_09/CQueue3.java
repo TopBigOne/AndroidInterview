812c4cd3efce40782e78218e93872bef7f9bf679
guangya.zhou <chinagyzhou@gmail.com>
2022-08-10 13:36:32 +0800

    offer 69 leetcode 169 : 用栈实现队列； add tail 的时候，reverse 两次；

--- Algorithm/src/main/java/stack/offer_09/CQueue3.java
+++ Algorithm/src/main/java/stack/offer_09/CQueue3.java
@@ -5,44 +5,33 @@ import java.util.Stack;
 /**
  * @author : dev
  * @version :
- * @Date :  3/4/21 11:07 PM
+ * @Date :  2022/8/10 13:18
  * @Desc :
  */
-public class CQueue {
+public class CQueue3 {
 
 
-    public static void main(String[] args) {
-        CQueue cQueue = new CQueue();
-        cQueue.appendTail(5);
-        cQueue.appendTail(2);
-
-        cQueue.deleteHead();
-        cQueue.deleteHead();
+    public CQueue3() {
 
     }
 
     Stack<Integer> normal = new Stack<>();
     Stack<Integer> reverse = new Stack<>();
 
-    public CQueue() {
-
-    }
-
-
-    // 在队尾插入
     public void appendTail(int value) {
         while (!reverse.isEmpty()) {
             normal.push(reverse.pop());
         }
+
         reverse.push(value);
 
         while (!normal.isEmpty()) {
             reverse.push(normal.pop());
         }
 
+
     }
 
-    // 删除队列头
     public int deleteHead() {
         if (reverse.isEmpty()) {
             return -1;
