b22f3b01e1ab4f3164d24554c8fbec95221b28de
guangya.zhou <chinagyzhou@gmail.com>
2023-07-13 19:30:50 +0800

    1: 测试一下java的substring ,主要目的是为了和C++做一个比较；

--- Algorithm/src/main/java/string/Test_Substring.java
+++ Algorithm/src/main/java/string/Test_Substring.java
@@ -4,11 +4,11 @@ package string;
  * @author : dev
  * @version :
  * @Date :  5/30/21 1:19 AM
- * @Desc : 测试：substring
+ * @Desc : 测试：substring  :  包含java和C++
  */
 public class Test_Substring {
     public static void main(String[] args) {
-        test1();
+        test_substring();
         test2();
 
     }
@@ -17,13 +17,13 @@ public class Test_Substring {
         String a = "12";
         String b = "34";
 
-      int s =  Integer.parseInt(String.valueOf(a.charAt(0)));
+        int s = Integer.parseInt(String.valueOf(a.charAt(0)));
         System.out.println("my_s: " + s);
     }
 
-    private static void test1() {
-        String s = "qwer";
-        String newStr = s.substring(1);
-        System.out.println(newStr);
+    private static void test_substring() {
+        String s = "qwertyuiop";
+        String newStr = s.substring(2, 7);
+        System.err.println("newStr : "+newStr);
     }
 }
