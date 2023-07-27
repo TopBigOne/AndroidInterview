21413937051a4b14f75f76299dd94bc57288fa61
guangya.zhou <chinagyzhou@gmail.com>
2023-07-27 23:35:49 +0800

    1: 温故换零钱！

--- Algorithm/src/main/java/packsack/leet_code_322/CoinChange8.java
+++ Algorithm/src/main/java/packsack/leet_code_322/CoinChange8.java
@@ -30,6 +30,7 @@ public class CoinChange8 {
 
     public int coinChange2(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
+        //
         Arrays.fill(dp, amount + 1);
         for (int i = 1; i <= amount; i++) {
             for (int coin : coins) {
@@ -43,6 +44,7 @@ public class CoinChange8 {
         if (dp[amount] == amount + 1) {
             return -1;
         }
+        // 11
         return dp[amount];
     }
 }
